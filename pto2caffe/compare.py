3446984fc01a88dd361d01dd95dbe7f714e112c5
chenkun <chenkun@vimicro.com>
2021-08-24 15:22:38 +0800

    Add comparing mode.

--- pto2caffe/compare.py
+++ pto2caffe/compare.py
@@ -1,53 +1,13 @@
-#import os
-#import sys
-#envroot = os.environ.get('MCHOME', os.environ['PWD'])
-#sys.path.append(envroot + 'toolchain/caffe2npu')
-#from compare_data import *
-#from load_data import LoadData
-#
-#def compare(platform, model_name, input_file_name):
-#    print('Comparing...')
-#    dump_path = envroot + '/dump/' + model_name
-#    target_data = dump_path +'/'+ platform +'/'+ input_file_name
-#    caffe_data =  dump_path + '/caffe/' + input_file_name
-#    data_load = LoadData()
-#
-#    if data_load.load_file(target_data, caffe_data) == False:
-#        print("Input directories are not match, please check")
-#
-#    while 1:
-#        file1, file2 = data_load.get_one_batch()
-#        if file1 == None or file2 == None:
-#            print('Dump File not found.')
-#            break
-#
-#        array1 = np.asarray(read_file(file1))
-#        array2 = np.asarray(read_file(file2))
-#        if len(array1) != len(array2):
-#            print("Get data error while calculate cosine similarity, lenght of 2 arrays are not equire!")
-#            continue
-#
-#        cosin_simi = calc_cosine_simi(array1, array2) # close 1 is better
-#        max_err = calc_max_diff(array1, array2) # close 0 is better
-#        max_ratio = calc_max_ratio(array1, array2) # close 0 is better
-#
-#        layer_name = os.path.split(file1)[1].split('_')[1]
-#        print(layer_name, ':')
-#        print('  cosin_simi: %8f'% cosin_simi)
-#        print('  cmax_err: %8f'% max_err)
-#        print('  max_ratio: %8f'% max_ratio, '\n')
-
 import os
 import sys
+import math
 envroot = os.environ.get('MCHOME', os.environ['PWD'])
 sys.path.append(envroot + 'toolchain/caffe/python')
 sys.path.append(envroot + 'toolchain/caffe/python/caffe')
+
 import caffe
-#import numpy as np
 from caffe.proto import caffe_pb2
 from google.protobuf import text_format
-
-import math
 
 
 def calc_size(array1, array2):
@@ -85,7 +45,7 @@ def calc_max_ratio(array1, array2):
     return max_ratio
 
 
-def dump_caffe_model(caffe_name, caffe_path, input_tensor):
+def dump_caffe_model(caffe_name, caffe_path, input_tensor, level):
     proto_file = caffe_path + '/' + caffe_name + '.prototxt'
     model_file = caffe_path + '/' + caffe_name + '.caffemodel'
     caffe_net = caffe.Net(proto_file, caffe.TEST, weights=model_file)
@@ -98,20 +58,31 @@ def dump_caffe_model(caffe_name, caffe_path, input_tensor):
 
     caffe_output_dict = dict()
     blob2layer_map = dict()
-    for i in range(len(caffe_net.layers)):
-        caffe_net._forward(i, i)
-        for idx in caffe_net._top_ids(i):
+
+    if level == 1:
+        caffe_net._forward(0, len(caffe_net.layers)-1)
+        for idx in caffe_net._top_ids(len(caffe_net.layers)-1):
             data = caffe_net._blobs[idx].data
             caffe_output_dict[caffe_net._blob_names[idx]] = data.reshape(-1)
-            blob2layer_map[caffe_net._blob_names[idx]] = caffe_net._layer_names[i]
+            blob2layer_map[caffe_net._blob_names[idx]] = caffe_net._layer_names[len(caffe_net.layers)-1]
+    elif level == 2:
+        for i in range(len(caffe_net.layers)):
+            caffe_net._forward(i, i)
+            for idx in caffe_net._top_ids(i):
+                data = caffe_net._blobs[idx].data
+                caffe_output_dict[caffe_net._blob_names[idx]] = data.reshape(-1)
+                blob2layer_map[caffe_net._blob_names[idx]] = caffe_net._layer_names[i]
 
     return caffe_output_dict, blob2layer_map
 
 
-def compare(platform, target_model, caffe_model, caffe_path, input_tensor):
-    print('Comparing...')
+def compare(platform, target_model, caffe_model, caffe_path, input_tensor, level=-1):
+    if level <= 0:
+        return
+    else:
+        print('Comparing...')
 
-    caffe_output_dict, blob2layer_map = dump_caffe_model(caffe_model, caffe_path, input_tensor)
+    caffe_output_dict, blob2layer_map = dump_caffe_model(caffe_model, caffe_path, input_tensor, level)
 
     if platform == 'tflite':
         from tflite2caffe.tflite_run import get_output
