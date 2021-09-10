ba2909aef925b7b693016b81ace329cd3bf76547
chenkun <chenkun@vimicro.com>
2021-09-10 16:19:05 +0800

    Update shape_map_nhwc2nchw

--- pto2caffe/util.py
+++ pto2caffe/util.py
@@ -11,14 +11,26 @@ trans_dtype = {
     'f32': np.float32
 }
 
+
 dtype_map = {
     'u8': 0,
     's16': 1,
     'f32': 2
 }
 
-def shape_map_nhwc2nchw(shape:list):
-    if isinstance(shape, np.ndarray) and len(shape) == 4:
-        return [shape[0], shape[3], shape[1], shape[2]]
+
+def shape_map_nhwc2nchw(shape):
+    if isinstance(shape, np.ndarray):
+        if len(shape) == 4:
+            return [shape[0], shape[3], shape[1], shape[2]]
+        elif len(shape) == 3:
+            return [shape[0], shape[1], shape[2]]
+        elif len(shape) == 2:
+            return [shape[0], shape[1]]
+        elif len(shape) == 1:
+            return [shape[0]]
+        else:
+            print(shape, shape.size, len(shape))
+            raise NotImplementedError(shape)
     else:
-        return list(shape)
+        return [shape]
