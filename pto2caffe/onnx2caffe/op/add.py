53017d343e0c053be26f3493fa0068cdec40f2ae
Kyle <onlyxool@gmail.com>
2021-12-18 15:56:11 +0800

    ONNX2Caffe: 1. Add Ops: Add Mul Sub

---
+++ pto2caffe/onnx2caffe/op/add.py
@@ -0,0 +1,79 @@
+import logging
+import numpy as np
+
+from caffe_transform import caffe_layer
+from onnx2caffe.op.operator import Operator
+
+logger = logging.getLogger('onnx2caffe')
+
+
+
+class Add(Operator):
+
+    def __init__(self, model, node, index):
+        super().__init__(model, node, index)
+        self.setInited()
+
+
+    @property
+    def type(self):
+        if hasattr(self, 'eltwise_param'):
+            return 'Eltwise'
+        elif hasattr(self, 'scale_param'):
+            return 'Scale'
+        else:
+            return self.op_code
+
+
+    def parse(self):
+        logger.debug("Parsing %s...", self.type)
+
+        self.parseInput()
+        self.parseOutput()
+
+        # Option
+        self.parseAttributes()
+
+        for legacy in self.model.legacys:
+            for i, input in enumerate(self.inputs):
+                if input == legacy.outputs[0] and legacy.type == 'Constant':
+                    self.inputs_buf[i] = legacy.inputs_buf[0]
+
+        if self.inputs_buf[0] is None and self.inputs_buf[1] is None:
+            self.eltwise_param = dict()
+            self.eltwise_param['operation'] = 1 # Caffe Eltwise SUM
+            self.attrs = self.eltwise_param
+        else:
+            self.weight = np.ones(self.inputs_shape[0], dtype=None, order='C')
+
+            if self.inputs_buf[0] is not None:
+                bias_index = 0
+                input_index = 1
+            else:
+                bias_index = 1
+                input_index = 0
+            self.bias = self.inputs_buf[bias_index]
+
+            self.scale_param = dict()
+            self.scale_param['bias_term'] = True
+            # Axis
+            if self.model.opset[0] >= 7:
+                self.scale_param['axis'] = self.inputs_shape[input_index].index(self.bias.shape[0])
+            else:
+                self.scale_param['axis'] = self.attrs['axis']
+
+            self.scale_param['num_axes'] = len(self.bias.shape)
+            self.attrs = self.scale_param
+
+        self.setParsed()
+
+
+    def convert(self):
+        if hasattr(self, 'eltwise_param'):
+            layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, eltwise_param=self.eltwise_param)
+        elif hasattr(self, 'scale_param'):
+            layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, self.weight, self.bias, scale_param=self.scale_param)
+
+        self.setConverted()
+
+        return [layer]
---
+++ pto2caffe/onnx2caffe/op/mul.py
@@ -0,0 +1,72 @@
+import logging
+
+from caffe_transform import caffe_layer
+from onnx2caffe.op.operator import Operator
+
+logger = logging.getLogger('onnx2caffe')
+
+
+
+class Mul(Operator):
+
+    def __init__(self, model, node, index):
+        super().__init__(model, node, index)
+        self.setInited()
+
+
+    @property
+    def type(self):
+        if hasattr(self, 'eltwise_param'):
+            return 'Eltwise'
+        elif hasattr(self, 'scale_param'):
+            return 'Scale'
+        else:
+            return self.op_code
+
+
+    def parse(self):
+        logger.debug("Parsing %s...", self.type)
+
+        self.parseInput()
+        self.parseOutput()
+
+        # Option
+        self.parseAttributes()
+
+        for legacy in self.model.legacys:
+            if legacy.outputs[0] == self.inputs[1] and legacy.type == 'Constant':
+                self.inputs_buf[1] = legacy.inputs_buf[0]
+
+        if self.inputs_buf[0] is None and self.inputs_buf[1] is None:
+            self.eltwise_param = dict()
+            self.eltwise_param['operation'] = 0 # Caffe Eltwise PROD
+            self.attrs = self.eltwise_param
+        else:
+            self.scale_param = dict()
+            self.scale_param['bias_term'] = False
+            # Axis
+            if self.model.opset[0] >= 7:
+                for i in range(len(self.inputs_shape[0])):
+                    if self.inputs_shape[0][i] == self.inputs_shape[1][0]:
+                        self.scale_param['axis'] = i
+                        break
+            else:
+                self.scale_param['axis'] = self.attrs['axis']
+
+            self.scale_param['num_axes'] = len(self.inputs_shape[1])
+            self.attrs = self.scale_param
+            self.weight = self.inputs_buf[1]
+            self.bias = None
+
+        self.setParsed()
+
+
+    def convert(self):
+        if hasattr(self, 'eltwise_param'):
+            layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, eltwise_param=self.eltwise_param)
+        elif hasattr(self, 'scale_param'):
+            layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, self.weight, self.bias, scale_param=self.scale_param)
+
+        self.setConverted()
+
+        return [layer]
