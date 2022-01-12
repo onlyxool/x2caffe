31f584b16876d8a2b709b298681ef74a223cb392
Kyle <onlyxool@gmail.com>
2022-01-12 15:43:06 +0800

    Add ONNX simplify

--- pto2caffe/onnx2caffe/convert.py
+++ pto2caffe/onnx2caffe/convert.py
@@ -4,8 +4,26 @@ from onnx2caffe.model import Model
 from caffe_dump import dump_caffe_model
 from compare import compare
 
+
+def set_batch_size(onnx_model):
+    inputs = onnx_model.graph.input
+    for input in inputs:
+        dim = input.type.tensor_type.shape.dim[0]
+        if dim.dim_value == 0:
+            dim.dim_value = 1
+
+
 def convert(onnx_file, input_tensor, caffe_model_path, dump_level=-1, param=None):
     onnx_model = onnx.load(onnx_file)
+
+    set_batch_size(onnx_model)
+
+    # ONNX Simplifier
+    if param.get('simplifier', 0) == 1:
+        from onnxsim import simplify
+        onnx_model, check = simplify(onnx_model)
+        assert check, "Simplified ONNX model could not be validated"
+
     onnx_model = shape_inference.infer_shapes(onnx_model)
     try:
         onnx.checker.check_model(onnx_model)
