fc5b8e9e73463f91aa0cd421deeb4b03e3f135db
Kyle <onlyxool@gmail.com>
2022-01-12 15:50:18 +0800

    ONNX2Caffe: Fix Bug pad compute

--- pto2caffe/onnx2caffe/op/deconv.py
+++ pto2caffe/onnx2caffe/op/deconv.py
@@ -26,7 +26,7 @@ class Deconvolution(Operator):
 
         # Weight
         self.weight = self.inputs_buf[1]
-        print(self.name, self.inputs_shape, self.outputs_shape)
+
         # Bias
         self.bias = self.inputs_buf[2] if len(self.inputs_buf) == 3 else None
 
@@ -40,21 +40,16 @@ class Deconvolution(Operator):
         self.convolution_param['bias_term'] = True if self.bias is not None else False
 
         # Padding
-        attr_padding = self.attrs.get('pads', [0,0,0,0])
+        pad_l,pad_r,pad_t,pad_b = self.attrs.get('pads', [0,0,0,0])
         for legacy in self.model.legacys:
             if legacy.outputs[0] == self.inputs[0] and legacy.op_code == 'Pad':
                 legacy_pad = legacy.pad
-                pad_l = attr_padding[1] + legacy.pad['left']
-                pad_r = attr_padding[3] + legacy.pad['right']
-                pad_t = attr_padding[0] + legacy.pad['top']
-                pad_b = attr_padding[2] + legacy.pad['bottom']
+                pad_l += legacy.pad['left']
+                pad_r += legacy.pad['right']
+                pad_t += legacy.pad['top']
+                pad_b += legacy.pad['bottom']
                 self.inputs[0] = legacy.inputs[0]
                 self.inputs_shape[0] = legacy.inputs_shape[0]
-        else:
-            pad_l = attr_padding[1]
-            pad_r = attr_padding[3]
-            pad_t = attr_padding[0]
-            pad_b = attr_padding[2]
 
         if pad_l == pad_r and pad_t == pad_b:
             self.convolution_param['pad_w'] = pad_l
--- pto2caffe/onnx2caffe/op/pooling.py
+++ pto2caffe/onnx2caffe/op/pooling.py
@@ -45,21 +45,16 @@ class Pooling(Operator):
         self.pooling_param['ceil_mode'] = self.attrs.get('ceil_mode', False)
 
         # Padding
-        attr_padding = self.attrs.get('pads', [0,0,0,0])
+        pad_l,pad_r,pad_t,pad_b = self.attrs.get('pads', [0,0,0,0])
         for legacy in self.model.legacys:
             if legacy.outputs[0] == self.inputs[0] and legacy.op_code == 'Pad':
                 legacy_pad = legacy.pad
-                pad_l = attr_padding[1] + legacy.pad['left']
-                pad_r = attr_padding[3] + legacy.pad['right']
-                pad_t = attr_padding[0] + legacy.pad['top']
-                pad_b = attr_padding[2] + legacy.pad['bottom']
+                pad_l += legacy.pad['left']
+                pad_r += legacy.pad['right']
+                pad_t += legacy.pad['top']
+                pad_b += legacy.pad['bottom']
                 self.inputs[0] = legacy.inputs[0]
                 self.inputs_shape[0] = legacy.inputs_shape[0]
-        else:
-            pad_l = attr_padding[1]
-            pad_r = attr_padding[3]
-            pad_t = attr_padding[0]
-            pad_b = attr_padding[2]
 
         if pad_l == pad_r and pad_t == pad_b:
             self.pooling_param['pad_w'] = pad_l
