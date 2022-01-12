0b5928d623e4535bb3bb19fb584df2cc14f4cff7
Kyle <onlyxool@gmail.com>
2022-01-12 18:10:35 +0800

    ONNX2Caffe: Support Slice ops

--- pto2caffe/onnx2caffe/op/slice.py
+++ pto2caffe/onnx2caffe/op/slice.py
@@ -5,17 +5,17 @@ from onnx2caffe.op.operator import Operator
 
 logger = logging.getLogger('onnx2caffe')
 
-class Cut(Operator):
+class Slice(Operator):
 
     def __init__(self, model, node, index):
         super().__init__(model, node, index)
-        self.cut_param = dict()
+        self.slice_param = dict()
         self.setInited()
 
 
     @property
     def type(self):
-        return 'Cut'
+        return 'Slice'
 
 
     def parse(self):
@@ -27,39 +27,49 @@ class Cut(Operator):
         # Option
         self.parseAttributes()
 
-        if self.inputs_buf[1].size <= 2: # Axes
-            axis = list(self.inputs_buf[3])
-            if min(axis) < 2 or max(axis) > 3: # Only support width and height
-                raise NotImplementedError
+        if self.model.opset[0] < 10:
+            num_slices = len(self.attr['starts'])
+            starts = self.attr['starts']
+            axes = self.attr['axes']
+            ends = self.attr['ends']
+            steps = [1]*num_slices
         else:
+            num_slices = len(self.inputs_buf[1])
+            starts = list(self.inputs_buf[1])
+            ends = list(self.inputs_buf[2])
+            axes = list(self.inputs_buf[3])
+            steps = list(self.inputs_buf[4]) if len(self.inputs_buf) >= 5 else [1]*num_slices
+
+        if num_slices > 1:
             raise NotImplementedError
 
-        starts = list(self.inputs_buf[1])
-        ends = list(self.inputs_buf[2])
-        steps = list(self.inputs_buf[4])
+        if max(steps) == 1:
+            axis_length = self.inputs_shape[0][axes[0]]
+            self.slice_param['axis'] = axes[0]
 
-        self.cut_param['axis'] = axis[0]
-        self.cut_param['offset'] = starts[0]
-        self.cut_param['height'] = steps[0]
-
-        if len(axis) > 1:
-            self.cut_param1 = dict()
-            self.cut_param1['axis'] = axis[1]
-            self.cut_param1['offset'] = starts[1]
-            self.cut_param1['width'] = steps[1]
-
-        self.attrs = self.cut_param
+            if starts[0] == 0:
+                self.slice_param['slice_point'] = [ends[0]]
+                self.outputs.append(self.name + 'useless')
+            elif ends[0] == axis_length:
+                self.slice_param['slice_point'] = [starts[0]]
+                self.outputs.insert(0, self.name + 'useless')
+            else:
+                self.slice_param['slice_point'] = [starts[0], ends[0]]
+                self.outputs.insert(0, self.name + 'useless0')
+                self.outputs.append(self.name + 'useless1')
+        else:
+            import sys
+            errorMsg = 'Do not support step > 1. ' + self.name + '\'s steps is ' + str(steps) + '\n'
+            sys.exit(errorMsg)
 
         self.setParsed()
 
 
     def convert(self):
         layers = []
-        layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, cut_param=self.cut_param)
+
+        layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, slice_param=self.slice_param)
         layers.append(layer)
-        if hasattr(self,'cut_param1'):
-            layer1 = caffe_layer(self.type, self.name, self.outputs, self.inputs_buf, self.outputs, cut_param=self.cut_param1)
-            layers.append(layer1)
 
         self.setConverted()
 
