aea9962255ba2247ac738a98b944c2b7a8b1ee35
Kyle <chenkun@vimicro.com>
2022-05-05 14:17:36 +0800

    Tensorflow2Caffe: Add Op MatMul

---
+++ pto2caffe/tensorflow2caffe/op/matmul.py
@@ -0,0 +1,57 @@
+import logging
+
+from caffe_transform import caffe_layer
+from tensorflow2caffe.op.operator import Operator
+
+
+logger = logging.getLogger('TensorFlow2Caffe')
+
+class MatMul(Operator):
+
+    def __init__(self, model, tf_op, tf_op_code, index):
+        super().__init__(model, tf_op, tf_op_code, index)
+        self.setInited()
+
+
+    @property
+    def type(self):
+        return 'InnerProduct'
+
+
+    def parse(self):
+        logger.debug('Parsing %s...', self.type)
+
+        self.parseInput()
+        self.parseOutput()
+
+        self.parseAttributes()
+
+        if len(self.inputs_shape[0]) == 2 and len(self.inputs_shape[1]) == 2:
+            if not self.attrs.get('transpose_b', False):
+                self.weight = self.inputs_buf[1].transpose(1,0)
+            else:
+                self.weight = self.inputs_buf[1]
+            self.bias = None
+
+            self.inner_product_param = dict()
+            self.inner_product_param['num_output'] = self.weight.shape[0]
+            self.inner_product_param['transpose'] = False
+
+            self.inner_product_param['weight_filler'] = dict()
+            self.inner_product_param['weight_filler']['type'] = 'constant' 
+
+            self.inner_product_param['bias_term'] = False
+
+            self.attrs = self.inner_product_param
+
+            self.setParsed()
+        else:
+            raise NotImplementedError('MatMul don\'t support input dimentions > 2')
+
+
+    def convert(self):
+        layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, self.weight, self.bias, inner_product_param=self.inner_product_param)
+
+        self.setConverted()
+
+        return [layer]
