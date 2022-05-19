859438ee49b11fc0a42bbebd6c448934e55c0977
Kyle <chenkun@vimicro.com>
2022-05-19 13:38:54 +0800

    Pytorch2Caffe: Add Op Transpose

---
+++ pto2caffe/pytorch2caffe/op/transpose.py
@@ -0,0 +1,38 @@
+from caffe_transform import caffe_layer
+from pytorch2caffe.op.operator import Operator
+
+
+class Transpose(Operator):
+
+    def __init__(self, model, pnnx, type_code, index):
+        super().__init__(model, pnnx, type_code, index)
+        assert(self.operator_code == 'torch.transpose')
+        self.setInited()
+
+
+    def parse(self):
+        self.layer_type = 'Permute'
+        super().__parse__()
+
+        order = list()
+        for i in range(len(self.inputs_shape[0])):
+            order.append(i)
+
+        order[self.attrs['dim0']] = self.attrs['dim1']
+        order[self.attrs['dim1']] = self.attrs['dim0']
+
+        # Attributes
+        self.permute_param = dict()
+        self.permute_param['order'] = order
+
+        self.attrs = self.permute_param
+
+        self.setParsed()
+
+
+    def convert(self):
+        layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, permute_param=self.permute_param)
+
+        self.setConverted()
+
+        return [layer]
