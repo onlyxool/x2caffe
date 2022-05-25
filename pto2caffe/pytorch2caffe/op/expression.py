7f650b18916db56bc29a08a31b9409e3985d518f
Kyle <chenkun@vimicro.com>
2022-05-25 19:32:15 +0800

    Pytorch2Caffe: Remove useless code in expression.py

--- pto2caffe/pytorch2caffe/op/expression.py
+++ pto2caffe/pytorch2caffe/op/expression.py
@@ -5,34 +5,6 @@ import numpy as np
 from caffe_transform import caffe_layer
 from pytorch2caffe.op.operator import Operator
 
-#from util import trim_one
-#from util import compute_scale_axis
-
-
-#def trim_one(inputs_shape):
-#    reshape = copy.deepcopy(inputs_shape)
-#    reshape_flag = []
-#    for i, input_shape in enumerate(inputs_shape):
-#        if input_shape == [] or input_shape is None:
-#            continue 
-#
-#        # Remove 1 from head
-#        while True:
-#            if input_shape[0] == 1:
-#                input_shape.remove(1)
-#            else:
-#                break
-#    
-#        # Remove 1 from tail
-#        while True:
-#            if input_shape[-1] == 1:
-#                input_shape.pop()
-#            else:
-#                break
-#        reshape_flag.append(not(len(reshape[i]) == len(input_shape)))
-#
-#    return reshape
-
 
 def trim_one(input_shape):
     ret = False
@@ -74,50 +46,6 @@ def need_switch(inputs_shape):
     else:
          return False
 
-#def trim_one(inputs_shape):
-#    reshape = copy.deepcopy(inputs_shape)
-#    for i, input_shape in enumerate(reshape):
-#        if input_shape == [] or input_shape is None:
-#            continue 
-#        # Remove 1 from tail
-#        while True:
-#            if input_shape[-1] == 1:
-#                input_shape.pop()
-#            else:
-#                break
-#    return reshape
-
-#def compute_scale_axis(bottom_shape, scale_shape):
-#    ''' 
-#    The first axis of bottom[0] (the first input Blob) along which to apply
-#    bottom[1] (the second input Blob).  May be negative to index from the end
-#    (e.g., -1 for the last axis).
-#
-#    For example, if bottom[0] is 4D with shape 100x3x40x60, the output
-#    top[0] will have the same shape, and bottom[1] may have any of the
-#    following shapes (for the given value of axis):
-#       (axis == 0 == -4) 100; 100x3; 100x3x40; 100x3x40x60
-#       (axis == 1 == -3)          3;     3x40;     3x40x60
-#       (axis == 2 == -2)                   40;       40x60
-#       (axis == 3 == -1)                                60
-#    Furthermore, bottom[1] may have the empty shape (regardless of the value of
-#    "axis") -- a scalar multiplier.
-#    '''
-#    if scale_shape == []: 
-#        return 0
-#
-#    shapeA = np.array(bottom_shape)
-#    shapeB = np.array(scale_shape)
-#
-#    print(shapeA, shapeB, '------------')
-#    for i in range(len(shapeA)):
-#        shape_map = (shapeA[i:(len(shapeB)+i)] == shapeB)
-#        print(shape_map, type(shape_map), type(np.array), len(shapeB))
-##        if (type(shape_map) == type(np.ndarray)) and shape_map.count(True) == len(shapeB):
-#        if isinstance(shape_map, np.ndarray) and list(shape_map).count(True) == len(shapeB):
-#            return i
-#
-#    return None
 
 def compute_scale_axis(bottom_shape, scale_shape):
     if not isinstance(bottom_shape, list) and not isinstance(bottom_shape, tuple):
@@ -135,29 +63,6 @@ def compute_scale_axis(bottom_shape, scale_shape):
         for colum in line:
             if colum == scale_shape:
                 return no
-
-
-#def compute_scale_axis(bottom_shape, scale_shape):
-#    if scale_shape == [] or scale_shape == ():
-#        return 0
-#
-#    bottom_shape_np = np.array(bottom_shape)
-#    scale_shape_np = np.array(scale_shape)
-#
-#    shape_flag = []
-#    for i in range(bottom_shape_np.size - scale_shape_np.size + 1):
-#        shape_map = list(bottom_shape_np[i:(scale_shape_np.size+i)] == scale_shape_np)
-#
-#        if i > 0:
-#            shape_map.insert(i, False)
-#
-#        for j in range(bottom_shape_np.size - len(shape_map)):
-#            shape_map.append(False)
-#
-#        if shape_map.count(True) > shape_flag.count(True):
-#            shape_flag = shape_map
-#
-#    return shape_flag
 
 
 class Expression(Operator):
