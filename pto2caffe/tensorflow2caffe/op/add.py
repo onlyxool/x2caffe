b7bac625e27d3ad973934eeecfb04708a9167fbd
Kyle <chenkun@vimicro.com>
2022-06-04 18:15:06 +0800

    Tensorflow2Caffe: Add -> Scale: None + numpy

--- pto2caffe/tensorflow2caffe/op/add.py
+++ pto2caffe/tensorflow2caffe/op/add.py
@@ -1,3 +1,4 @@
+import numpy as np
 from caffe_transform import caffe_layer
 from tensorflow2caffe.op.operator import Operator
 
@@ -11,18 +12,51 @@ class Add(Operator):
 
 
     def parse(self):
-        self.layer_type = 'Eltwise'
         super().__parse__()
 
-        self.eltwise_param = dict()
-        self.eltwise_param['operation'] = 1
-        self.attrs = self.eltwise_param
+        if self.inputs_buf[0] is None and self.inputs_buf[1] is None:
+            self.layer_type = 'Eltwise'
+            self.eltwise_param = dict()
+            self.eltwise_param['operation'] = 1
+
+            self.attrs = self.eltwise_param
+        else:
+            self.layer_type = 'Scale'
+
+            if self.inputs_buf[0] is not None:
+                bias_index = 0
+                input_index = 1
+            else:
+                bias_index = 1
+                input_index = 0
+
+            # Weight
+            self.weight = np.ones(self.inputs_shape[bias_index], dtype=float, order='C')
+
+            # Bias
+            self.bias = self.inputs_buf[bias_index]
+
+            # Attributes
+            self.scale_param = dict()
+            self.scale_param['bias_term'] = True
+
+            # Axis
+            if self.bias.shape != () and self.bias.shape != []:
+                self.scale_param['axis'] = self.inputs_shape[input_index].index(self.bias.shape[0])
+                self.scale_param['num_axes'] = len(self.bias.shape)
+            else:
+                self.scale_param['num_axes'] = 0
+
+            self.attrs = self.scale_param
 
         self.setParsed()
 
 
     def convert(self):
-        layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, eltwise_param=self.eltwise_param)
+        if self.type == 'Eltwise':
+            layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, eltwise_param=self.eltwise_param)
+        elif self.type == 'Scale':
+            layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, self.weight, self.bias, scale_param=self.scale_param)
 
         self.setConverted()
 
