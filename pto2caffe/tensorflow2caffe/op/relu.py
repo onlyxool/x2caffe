7cfe7fcb855b0702f3e5db42cb54d6c1b4687c58
Kyle <chenkun@vimicro.com>
2022-06-04 18:28:10 +0800

    Tensorflow2Caffe: Add Op ReLU

---
+++ pto2caffe/tensorflow2caffe/op/relu.py
@@ -0,0 +1,30 @@
+from caffe_transform import caffe_layer
+from tensorflow2caffe.op.operator import Operator
+
+
+class ReLU(Operator):
+
+    def __init__(self, model, tf_op, index):
+        super().__init__(model, tf_op, index)
+        assert(self.operator_code == 'Relu')
+        self.setInited()
+
+
+    def parse(self):
+        self.layer_type = 'ReLU'
+        super().__parse__()
+
+        self.relu_param = dict()
+        self.relu_param['negative_slope'] = 0
+
+        self.attrs = self.relu_param
+
+        self.setParsed()
+
+
+    def convert(self):
+        layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, relu_param=self.relu_param)
+
+        self.setConverted()
+
+        return [layer]
