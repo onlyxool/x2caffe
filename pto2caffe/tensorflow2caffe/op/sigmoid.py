f091307c2ae4ef8b1f12f3f9cc907c43cbbaca93
Kyle <chenkun@vimicro.com>
2022-07-05 13:46:05 +0800

    Tensorflow2Caffe: Add Op Sigmoid

---
+++ pto2caffe/tensorflow2caffe/op/sigmoid.py
@@ -0,0 +1,28 @@
+from caffe_transform import caffe_layer
+from tensorflow2caffe.op.operator import Operator
+
+
+class Sigmoid(Operator):
+
+    def __init__(self, model, tf_op, index):
+        super().__init__(model, tf_op, index)
+        assert(self.operator_code == 'Sigmoid')
+        self.setInited()
+
+
+    def parse(self):
+        self.layer_type = 'Sigmoid'
+        super().__parse__()
+
+        self.sigmoid_param = dict()
+        self.attrs = self.sigmoid_param
+
+        self.setParsed()
+
+
+    def convert(self):
+        layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, sigmoid_param=self.sigmoid_param)
+
+        self.setConverted()
+
+        return [layer]
