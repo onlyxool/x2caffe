114d392a25a144169ddde1681e7328e3eb4d513e
Kyle <chenkun@vimicro.com>
2022-07-06 16:55:54 +0800

    Tensorflow2Caffe: Fix Bug in Sub

--- pto2caffe/tensorflow2caffe/op/sub.py
+++ pto2caffe/tensorflow2caffe/op/sub.py
@@ -1,5 +1,8 @@
+import numpy as np
 from caffe_transform import caffe_layer
 from tensorflow2caffe.op.operator import Operator
+
+from util import compute_scale_axis
 
 
 class Sub(Operator):
@@ -13,36 +16,45 @@ class Sub(Operator):
     def parse(self):
         super().__parse__()
 
-        if self.inputs_buf[0] is None and self.inputs_buf[1] is None:
+        if self.inputs_buf[0] is not None and self.inputs_buf[1] is not None:
+            self.model.constant[self.outputs[0]] = self.inputs_buf[0] - self.inputs_buf[1]
+        elif self.inputs_buf[0] is None and self.inputs_buf[1] is None and self.inputs_shape[0] == self.inputs_shape[1]:
             self.layer_type = 'Eltwise'
             self.eltwise_param = dict()
             self.eltwise_param['operation'] = 3 # Caffe Eltwise SUB
             self.attrs = self.eltwise_param
-        else:
+            self.setParsed()
+        elif self.inputs_buf[0] is None and self.inputs_buf[1] is not None:
             self.layer_type = 'Scale'
 
-            self.bias = -self.inputs_buf[1]
+            # Weight
+            self.weight = np.ones(self.inputs_shape[0]).astype(np.float32)
 
+            # Bias
+            if len(self.inputs_shape[1]) == 4:
+                self.bias = -self.inputs_buf[1].transpose(0,3,1,2)
+            elif len(self.inputs_shape[1]) == 3:
+                self.bias = -self.inputs_buf[1].transpose(2,0,1)
+            else:
+                self.bias = -self.inputs_buf[1]
+
+            # Attribute
             self.scale_param = dict()
+            self.scale_param['axis'] = 0
+            self.scale_param['num_axes'] = len(self.weight.shape)
             self.scale_param['bias_term'] = True
 
-            # Axis
-            if self.bias.shape != () and self.bias.shape != []: 
-                self.scale_param['axis'] = self.inputs_shape[0].index(self.bias.shape[0])
-                self.scale_param['num_axes'] = len(self.bias.shape)
-            else:
-                self.scale_param['num_axes'] = 0 
-
             self.attrs = self.scale_param
-
-        self.setParsed()
+            self.setParsed()
+        else:
+            raise NotImplementedError
 
 
     def convert(self):
         if self.layer_type == 'Eltwise':
             layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, eltwise_param=self.eltwise_param)
         if self.layer_type == 'Scale':
-            layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, None, self.bias, scale_param=self.scale_param)
+            layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, self.weight, self.bias, scale_param=self.scale_param)
 
         self.setConverted()
 
