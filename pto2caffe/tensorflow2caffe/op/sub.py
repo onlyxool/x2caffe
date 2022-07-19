1dfd0eab78bfec17347ef6ddf74f0af25e0f5583
Kyle <chenkun@vimicro.com>
2022-07-19 20:41:47 +0800

    Tensorflow2Caffe: Op Sub: Handle constant - Var

--- pto2caffe/tensorflow2caffe/op/sub.py
+++ pto2caffe/tensorflow2caffe/op/sub.py
@@ -2,7 +2,7 @@ import numpy as np
 from caffe_transform import caffe_layer
 from tensorflow2caffe.op.operator import Operator
 
-from util import compute_scale_axis
+from util import get_layout
 
 
 class Sub(Operator):
@@ -31,10 +31,10 @@ class Sub(Operator):
             self.weight = np.ones(self.inputs_shape[0]).astype(np.float32)
 
             # Bias
-            if len(self.inputs_shape[1]) == 4:
-                self.bias = -self.inputs_buf[1].transpose(0,3,1,2)
-            elif len(self.inputs_shape[1]) == 3:
-                self.bias = -self.inputs_buf[1].transpose(2,0,1)
+            if len(self.inputs_shape[1]) == 4 and self.layout == 'NHWC':
+                self.bias = -self.inputs_buf[1].transpose(0, 3, 1, 2)
+            elif len(self.inputs_shape[1]) == 3 and get_layout(self.op.inputs[1].shape.as_list()) == 'HWX':
+                self.bias = -self.inputs_buf[1].transpose(2, 0, 1)
             else:
                 self.bias = -self.inputs_buf[1]
 
@@ -46,16 +46,58 @@ class Sub(Operator):
 
             self.attrs = self.scale_param
             self.setParsed()
+        elif self.inputs_buf[0] is not None and self.inputs_buf[1] is None:
+            self.layer_type = 'Scale+Scale'
+
+            self.inputs.reverse()
+            self.inputs_shape.reverse()
+            self.inputs_buf.reverse()
+
+            self.scale0 = 'Scale_split_sub'+str(self.index)
+            self.scale1 = 'Scale_split_neg'+str(self.index)
+            self.scale0_outputs = ['Scale_split_sub'+str(self.index)]
+            self.scale1_inputs = ['Scale_split_sub'+str(self.index), 'Scale_split_neg_weight']
+
+            # Weight
+            self.weight0 = np.ones(self.inputs_shape[0]).astype(np.float32)
+            self.weight1 = np.ones(self.outputs_shape[0]).astype(np.float32) * -1
+
+            # Bias
+            if len(self.inputs_shape[1]) == 4:
+                self.bias0 = -self.inputs_buf[1].transpose(0,3,1,2)
+            elif len(self.inputs_shape[1]) == 3 and get_layout(self.inputs_shape[1]) == 'HWX':
+                self.bias0 = -self.inputs_buf[1].transpose(2,0,1)
+            else:
+                self.bias0 = -self.inputs_buf[1]
+            self.bias1 = None
+
+            # Attribute
+            self.scale_param0 = dict()
+            self.scale_param0['axis'] = 0
+            self.scale_param0['num_axes'] = len(self.weight0.shape)
+            self.scale_param0['bias_term'] = True
+
+            self.scale_param1 = dict()
+            self.scale_param1['axis'] = 0
+            self.scale_param1['num_axes'] = len(self.weight1.shape)
+            self.scale_param1['bias_term'] = False
+
+            self.attrs = self.scale_param0
+            self.setParsed()
         else:
             raise NotImplementedError
 
 
     def convert(self):
+        layers = list()
         if self.layer_type == 'Eltwise':
-            layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, eltwise_param=self.eltwise_param)
+            layers.append(caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, eltwise_param=self.eltwise_param))
         if self.layer_type == 'Scale':
-            layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, self.weight, self.bias, scale_param=self.scale_param)
+            layers.append(caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, self.weight, self.bias, scale_param=self.scale_param))
+        if self.layer_type == 'Scale+Scale':
+            layers.append(caffe_layer('Scale', self.scale0, self.inputs, self.inputs_buf, self.scale0_outputs, self.weight0, self.bias0, scale_param=self.scale_param0))
+            layers.append(caffe_layer('Scale', self.scale1, self.scale1_inputs, [None, self.weight1], self.outputs, self.weight1, self.bias1, scale_param=self.scale_param1))
 
         self.setConverted()
 
-        return [layer]
+        return layers
