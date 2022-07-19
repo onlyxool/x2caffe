0905b33e355f6cf8e48ab838131f66b1dfca77ad
Kyle <chenkun@vimicro.com>
2022-07-19 20:52:50 +0800

    Tensorflow2Caffe: Add Op Transpose

---
+++ pto2caffe/tensorflow2caffe/op/transpose.py
@@ -0,0 +1,34 @@
+from caffe_transform import caffe_layer
+from tensorflow2caffe.op.operator import Operator
+
+
+class Transpose(Operator):
+
+    def __init__(self, model, tf_op, index):
+        super().__init__(model, tf_op, index)
+        assert(self.operator_code == 'Transpose')
+        self.setInited()
+
+
+    def parse(self):
+        self.layer_type = 'Permute'
+        super().__parse__()
+
+        if self.inputs_buf[0] is not None:
+            self.model.constant[self.outputs[0]] = self.inputs_buf[0].transpose(self.inputs_buf[1].tolist())
+
+        else:
+            # Attributes 
+            self.permute_param = dict()
+            self.permute_param['order'] = self.inputs_buf[1].tolist()
+            self.attrs = self.permute_param
+
+            self.setParsed()
+
+
+    def convert(self):
+        layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, permute_param=self.permute_param)
+
+        self.setConverted()
+
+        return [layer]
