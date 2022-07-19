b35cf7ccfb42500bcde43e46d4014432e1e435fc
Kyle <chenkun@vimicro.com>
2022-07-19 19:48:01 +0800

    Tensorflow2Caffe: In Op Realdiv: Transpose weight when it has multiple dims

--- pto2caffe/tensorflow2caffe/op/realdiv.py
+++ pto2caffe/tensorflow2caffe/op/realdiv.py
@@ -1,6 +1,8 @@
 from caffe_transform import caffe_layer
 from tensorflow2caffe.op.operator import Operator
 
+from util import get_layout
+
 
 class RealDiv(Operator):
 
@@ -23,15 +25,24 @@ class RealDiv(Operator):
                 self.inputs_shape.reverse()
                 self.inputs_buf.reverse()
 
+            # Scale Parameter
             self.scale_param = dict()
-
             if len(self.inputs_shape[1]) > 0:
                 self.scale_param['axis'] = self.inputs_shape[0].index(self.inputs_shape[1][0])
-
+            else:
+                self.scale_param['axis'] = 0
             self.scale_param['num_axes'] = len(self.inputs_shape[1])
             self.scale_param['bias_term'] = False
 
-            self.weight = 1 / self.inputs_buf[1]
+            # Wegith
+            if len(self.inputs_shape[1]) == 4:
+                self.weight = 1 / self.inputs_buf[1].transpose(0, 3, 1, 2)
+            elif len(self.inputs_shape[1]) == 3 and get_layout(self.op.inputs[1].shape.as_list()) == 'HWX':
+                self.weight = 1 / self.inputs_buf[1].transpose(2, 0, 1)
+            else:
+                self.weight = 1 / self.inputs_buf[1]
+
+            # Bias
             self.bias = None
 
             self.attrs = self.scale_param
