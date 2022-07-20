b5e35f60add2796bd7e4b9ed2c5d524a8ddd66e6
Kyle <chenkun@vimicro.com>
2022-07-20 10:56:46 +0800

    Tensorflow2Caffe: Add Op MaxPoolWithArgmax

---
+++ pto2caffe/tensorflow2caffe/op/maxpoolwithargmax.py
@@ -0,0 +1,42 @@
+from caffe_transform import caffe_layer
+from tensorflow2caffe.op.operator import Operator
+from util import handleLegacyPad, getLegacyAttrs
+
+
+class MaxPoolWithArgmax(Operator):
+
+    def __init__(self, model, tf_op, index):
+        super().__init__(model, tf_op, index)
+        assert(self.operator_code == 'MaxPoolWithArgmax')
+        self.setInited()
+
+
+    def parse(self):
+        self.layer_type = 'Pooling'
+        super().__parse__()
+
+        # Attribute
+        self.pooling_param = dict()
+        self.pooling_param['pool'] = 0
+        self.pooling_param['kernel_h'] = self.attrs['ksize'][self.ndim('H')]
+        self.pooling_param['kernel_w'] = self.attrs['ksize'][self.ndim('W')]
+        self.pooling_param['stride_h'] = self.attrs['strides'][self.ndim('H')]
+        self.pooling_param['stride_w'] = self.attrs['strides'][self.ndim('W')]
+        self.pooling_param['ceil_mode'] = True if self.attrs['padding'] == 'SAME' else False
+
+        # Padding
+        legacy_pad = getLegacyAttrs(self, 'Pad')
+        padding = handleLegacyPad(self.attrs['padding'], self.inputs_shape[0], self.outputs_shape[0], self.pooling_param, legacy_pad, self.type)
+        self.pooling_param.update(padding)
+
+        self.attrs = self.pooling_param
+
+        self.setParsed()
+
+
+    def convert(self):
+        layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, pooling_param=self.pooling_param)
+
+        self.setConverted()
+
+        return [layer]
