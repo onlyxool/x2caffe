006173dca2014bb0c89ce0afe2115caa2c46a352
Kyle <chenkun@vimicro.com>
2022-07-21 18:43:13 +0800

    Tensorflow2Caffe: Handle split num == 1 in Op StridedSlice

--- pto2caffe/tensorflow2caffe/op/stridedslice.py
+++ pto2caffe/tensorflow2caffe/op/stridedslice.py
@@ -15,30 +15,32 @@ class StridedSlice(Operator):
 
 
     def parse(self):
-        self.layer_type = 'Slice'
         super().__parse__()
 
         if self.inputs_buf[3].size == list(self.inputs_buf[3]).count(1):
-
-            self.slice_param = dict()
-
             axis_index = np.nonzero(self.inputs_buf[2] - self.inputs_buf[1])[0]
             if axis_index.size > 1:
                 errorMsg = 'Error[' + self.op.name + ']: Can\'t slice more than one axis'
                 sys.exit()
-            else:
-                axis_nhwc = int(axis_index[0])
-                self.slice_param['axis'] = dim_map_nhwc2nchw[axis_nhwc]
 
-            self.slice_param['slice_point'] = list(np.absolute(self.inputs_buf[2] - self.inputs_buf[1]))[axis_nhwc]
-            self.outputs.append(self.name+'_useless')
+            axis_nhwc = int(axis_index[0])
+            axis_nchw = dim_map_nhwc2nchw[axis_nhwc]
+
+            if self.inputs_buf[2][axis_nhwc] == self.op.inputs[0].shape[axis_nhwc]:
+                self.model.indentity[self.op.outputs[0].name] = self.model.indentity.get(self.op.inputs[0].name, self.op.inputs[0].name)
+            else:
+                self.layer_type = 'Slice'
+                self.slice_param = dict()
+                self.slice_param['axis'] = dim_map_nhwc2nchw[axis_nhwc]
+                self.slice_param['slice_point'] = list(np.absolute(self.inputs_buf[2] - self.inputs_buf[1]))[axis_nhwc]
+                self.outputs.append(self.name+'_useless')
+                self.attrs = self.slice_param
+                self.setParsed()
         else:
             errorMsg = 'Error[' + self.op.name + ']: Do not support stride > 1. OP ' + self.op.name + '\'s strides is ' + str(self.inputs_buf[3]) + '\n'
             sys.exit(errorMsg)
 
-        self.attrs = self.slice_param
 
-        self.setParsed()
 
 
     def convert(self):
