bdf21caa0fca8bd4a530ea497e4bd3bb3b2259ec
Kyle <chenkun@vimicro.com>
2022-07-26 14:52:10 +0800

    Tensorflow2Caffe: Add Op Sum support

---
+++ pto2caffe/tensorflow2caffe/op/sum.py
@@ -0,0 +1,30 @@
+from caffe_transform import caffe_layer
+from tensorflow2caffe.op.operator import Operator
+
+
+class Sum(Operator):
+
+    def __init__(self, model, tf_op, index):
+        super().__init__(model, tf_op, index)
+        assert(self.operator_code == 'Sum')
+        self.setInited()
+
+
+    def parse(self):
+        self.layer_type = 'Sum'
+        super().__parse__()
+
+        axes = self.inputs_buf[1]
+
+        # Handle Constant OP
+        if self.inputs_buf[0] is not None and self.inputs_buf[1] is not None:
+            import numpy as np
+            self.model.constant[self.outputs[0]] = np.sum(self.inputs_buf[0], axis=tuple(axes), keepdims=self.attrs['keep_dims'])
+        else:
+            raise NotImplementedError(self.op.name)
+
+        self.setParsed()
+
+
+    def convert(self):
+        pass
