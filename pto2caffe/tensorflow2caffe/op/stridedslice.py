7213ebfc881c41a186a20130e28559ece60e0886
Kyle <chenkun@vimicro.com>
2022-07-28 15:39:57 +0800

    Tensorflow2Caffe: 1. Fix Bug in Op StridedSlice 2. Handle two slice point

--- pto2caffe/tensorflow2caffe/op/stridedslice.py
+++ pto2caffe/tensorflow2caffe/op/stridedslice.py
@@ -23,32 +23,51 @@ class StridedSlice(Operator):
                 end = int(self.inputs_buf[2])
                 strides = int(self.inputs_buf[3])
                 self.model.constant[self.outputs[0]] = self.inputs_buf[0][begin:end:strides]
+            elif self.inputs_buf[0].ndim == 2:
+                begin = self.inputs_buf[1]
+                end = self.inputs_buf[2]
+                strides = self.inputs_buf[3]
+                self.model.constant[self.outputs[0]] = self.inputs_buf[0][begin[0]:end[0]:strides[0], begin[1]:end[1]:strides[1]]
             else:
-                raise NotImplementedError
+                raise NotImplementedError(self.op.name)
         else:
-            if self.inputs_buf[3].size == list(self.inputs_buf[3]).count(1):
-                axis_index = np.nonzero(self.inputs_buf[2] - self.inputs_buf[1])[0]
-                if axis_index.size > 1:
-                    errorMsg = 'Error[' + self.op.name + ']: Can\'t slice more than one axis'
-                    sys.exit()
-
-                axis = int(axis_index[0]) if self.layout == 'NHWC' else dim_map_nhwc2nchw[int(axis_index[0])]
-
-                if self.inputs_buf[2][axis] == self.op.inputs[0].shape[axis]:
-                    self.model.indentity[self.op.outputs[0].name] = self.model.indentity.get(self.op.inputs[0].name, self.op.inputs[0].name)
-                else:
-                    self.layer_type = 'Slice'
-                    self.slice_param = dict()
-                    self.slice_param['axis'] = axis
-                    self.slice_param['slice_point'] = list(np.absolute(self.inputs_buf[2] - self.inputs_buf[1]))[axis]
-                    self.outputs.append(self.name+'_useless')
-                    self.attrs = self.slice_param
-                    self.setParsed()
+            # Skip op if input shape == output shape
+            if self.op.inputs[0].shape == self.op.outputs[0].shape: # Skip
+                self.model.indentity[self.op.outputs[0].name] = self.model.indentity.get(self.op.inputs[0].name, self.op.inputs[0].name)
             else:
-                errorMsg = 'Error[' + self.op.name + ']: Do not support stride > 1. OP ' + self.op.name + '\'s strides is ' + str(self.inputs_buf[3]) + '\n'
-                sys.exit(errorMsg)
+                self.layer_type = 'Slice'
 
+                # Check Stride != 1
+                if self.inputs_buf[3].size != list(self.inputs_buf[3]).count(1):
+                    errorMsg = 'Error Op' + self.op.name + '(StridedSlice): Do not support stride > 1. OP ' + self.op.name + '\'s strides is ' + str(self.inputs_buf[3]) + '\n'
+                    sys.exit(errorMsg)
 
+                axis_index = np.nonzero(self.inputs_buf[2] - self.inputs_buf[1])[0]
+
+                if axis_index.size > 1:
+                    errorMsg = 'Error Op ' + self.op.name + '(StridedSlice): Can\'t slice more than one axis'
+                    sys.exit(errorMsg)
+
+                start = int(self.inputs_buf[1][axis_index[0]])
+                end = int(self.inputs_buf[2][axis_index[0]])
+
+                if start == 0:
+                    slice_point = end
+                    self.outputs.append(self.name+'_useless')
+                elif end == self.op.inputs[0].shape[axis_index[0]]:
+                    slice_point = start
+                    self.outputs.insert(0, self.name+'_useless')
+                else:
+                    errorMsg = 'Error Op ' + self.op.name + '(StridedSlice): Can\'t support begin: ' + str(self.inputs_buf[1]) + ' end: ' + str(self.inputs_buf[2])
+                    sys.exit(errorMsg)
+
+                self.slice_param = dict()
+                self.slice_param['axis'] = int(axis_index[0]) if self.layout == 'NCHW' else dim_map_nhwc2nchw[int(axis_index[0])]
+                self.slice_param['slice_point'] = [slice_point]
+
+                self.attrs = self.slice_param
+
+                self.setParsed()
 
 
     def convert(self):
