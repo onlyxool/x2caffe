f0fb3729e0c99888c43491d9e4f8cae0b81c9c8c
Kyle <chenkun@vimicro.com>
2022-07-28 15:33:02 +0800

    Tensorflow2Caffe: Add Op Switch

---
+++ pto2caffe/tensorflow2caffe/op/switch.py
@@ -0,0 +1,28 @@
+from caffe_transform import caffe_layer
+from tensorflow2caffe.op.operator import Operator
+
+
+class Switch(Operator):
+
+    def __init__(self, model, tf_op, index):
+        super().__init__(model, tf_op, index)
+        assert(self.operator_code == 'Switch')
+        self.setInited()
+
+
+    def parse(self):
+        self.layer_type = 'Switch'
+        super().__parse__()
+
+        if self.inputs_buf[0] is not None:
+            # Constant Op
+            self.model.constant[self.outputs[1]] = self.inputs_buf[0]
+            self.model.constant[self.outputs[0]] = self.inputs_buf[0]
+        else:
+            # Skip Op
+            self.model.indentity[self.op.outputs[1].name] = self.model.indentity.get(self.op.inputs[0].name, self.op.inputs[0].name)
+            self.model.indentity[self.op.outputs[0].name] = self.model.indentity.get(self.op.inputs[0].name, self.op.inputs[0].name)
+
+
+    def convert(self):
+        pass
