6b63df3e66eecda249852741dac5d6d88125dbf5
Kyle <chenkun@vimicro.com>
2022-08-06 18:55:42 +0800

    Tensorflow2Caffe: Add Op TensorListReserve

---
+++ pto2caffe/tensorflow2caffe/op/tensorlist.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+from tensorflow2caffe.op.operator import Operator
+
+
+class TensorList(Operator):
+
+    def __init__(self, model, tf_op, index):
+        super().__init__(model, tf_op, index)
+        assert(self.operator_code in ('TensorListReserve'))
+        self.setInited()
+
+
+    def parse(self):
+        super().__parse__()
+
+        if self.operator_code == 'TensorListReserve':
+            element_shape = tf.constant(self.inputs_buf[0], dtype=self.op.inputs[0].dtype)
+            num_elements = tf.constant(self.inputs_buf[1], dtype=self.op.inputs[1].dtype)
+            element_dtype = self.attrs['element_dtype']
+            self.model.constant[self.outputs[0]] = tf.raw_ops.TensorListReserve(element_shape=element_shape, num_elements=num_elements, element_dtype=element_dtype, name=None)
+
+
+    def convert(self):
+        pass 
