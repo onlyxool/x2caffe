a32dac47fcbf0ceff832220457cea21d9592a9fc
Kyle <chenkun@vimicro.com>
2022-08-06 18:51:50 +0800

    Tensorflow2Caffe: Add Ops GreaterEqual Greater Less Equal

---
+++ pto2caffe/tensorflow2caffe/op/compare.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+
+from tensorflow2caffe.op.operator import Operator
+
+
+class Compare(Operator):
+
+    def __init__(self, model, tf_op, index):
+        super().__init__(model, tf_op, index)
+        assert(self.operator_code in ('GreaterEqual', 'Greater', 'Less', 'Equal'))
+        self.setInited()
+
+
+    def parse(self):
+        self.layer_type = self.operator_code
+        super().__parse__()
+
+        if self.inputs_buf[0] is not None and self.inputs_buf[1] is not None:
+            x = tf.constant(self.inputs_buf[0], dtype=self.op.inputs[0].dtype)
+            y = tf.constant(self.inputs_buf[1], dtype=self.op.inputs[1].dtype)
+            if self.operator_code == 'GreaterEqual':
+                self.model.constant[self.outputs[0]] = tf.math.greater_equal(x, y).numpy()
+            elif self.operator_code == 'Greater':
+                self.model.constant[self.outputs[0]] = tf.math.greater(x, y).numpy()
+            elif self.operator_code == 'Less':
+                self.model.constant[self.outputs[0]] = tf.math.less(x, y).numpy()
+            elif self.operator_code == 'Equal':
+                self.model.constant[self.outputs[0]] = tf.math.equal(x, y).numpy()
+        else:
+            import sys
+            errorMsg = 'Error: Operator [' + self.operator_code + '] does not Support.\n'
+            sys.exit(errorMsg)
+
+
+    def convert(self):
+        pass
