43caa20c13cd328639bb664525406a0ecfcb419f
Kyle <chenkun@vimicro.com>
2022-08-06 18:56:12 +0800

    Tensorflow2Caffe: Add Op SelectV2

---
+++ pto2caffe/tensorflow2caffe/op/select.py
@@ -0,0 +1,28 @@
+from tensorflow2caffe.op.operator import Operator
+
+
+class Select(Operator):
+
+    def __init__(self, model, tf_op, index):
+        super().__init__(model, tf_op, index)
+        assert(self.operator_code == 'SelectV2')
+        self.setInited()
+
+
+    def parse(self):
+        super().__parse__()
+
+        if self.inputs_buf[0] is not None:
+            import tensorflow as tf
+            condition = tf.constant(self.inputs_buf[0], dtype=self.op.inputs[0].dtype)
+            t = tf.constant(self.inputs_buf[1], dtype=self.op.inputs[1].dtype)
+            e = tf.constant(self.inputs_buf[2], dtype=self.op.inputs[2].dtype)
+            self.model.constant[self.outputs[0]] = tf.raw_ops.SelectV2(condition=condition, t=t, e=e, name=None)
+        else:
+            import sys
+            errorMsg = 'Error: Operator [ SelectV2 ] does not Support.\n'
+            sys.exit(errorMsg)
+
+
+    def convert(self):
+        pass
