192bb70f1f7fb35cf31004898fe17f00c4d63bb6
Kyle <chenkun@vimicro.com>
2022-08-07 22:34:07 +0800

    Tensorflow2Caffe: Add Op Prod

---
+++ pto2caffe/tensorflow2caffe/op/prod.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+from tensorflow2caffe.op.operator import Operator
+
+
+class Prod(Operator):
+
+    def __init__(self, model, tf_op, index):
+        super().__init__(model, tf_op, index)
+        assert(self.operator_code == 'Prod')
+        self.setInited()
+
+
+    def parse(self):
+        self.layer_type = 'Prod'
+        super().__parse__()
+
+        if self.inputs_buf[0] is not None:
+            input = tf.constant(self.inputs_buf[0], dtype=self.op.inputs[0].dtype)
+            axis = tf.constant(self.inputs_buf[1], dtype=self.op.inputs[1].dtype)
+            self.model.constant[self.outputs[0]] = tf.raw_ops.Prod(input=input, axis=axis, keep_dims=self.attrs['keep_dims'], name=None)
+        else:
+            self.model.unsupport.append(self.operator_code)
+
+
+    def convert(self):
+        pass
