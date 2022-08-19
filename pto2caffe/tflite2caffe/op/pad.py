2aefce2d59e5bf777d21b0394460f80e9f668848
Kyle <chenkun@vimicro.com>
2022-08-19 16:48:10 +0800

    TFLite2Caffe: Check Channel Pad in Op Pad

--- pto2caffe/tflite2caffe/op/pad.py
+++ pto2caffe/tflite2caffe/op/pad.py
@@ -24,12 +24,16 @@ class Pad(Operator):
 
         if np.count_nonzero(self.inputs_buf[1]) > 0:
             pad = dict()
-            pad['left']   = self.inputs_buf[1][2][0]
-            pad['right']  = self.inputs_buf[1][2][1]
-            pad['top']    = self.inputs_buf[1][1][0]
-            pad['bottom'] = self.inputs_buf[1][1][1]
-
+            pad['left']   = self.inputs_buf[1][2][0].tolist()
+            pad['right']  = self.inputs_buf[1][2][1].tolist()
+            pad['top']    = self.inputs_buf[1][1][0].tolist()
+            pad['bottom'] = self.inputs_buf[1][1][1].tolist()
+            pad['channel'] = self.inputs_buf[1][3].tolist()
             self.model.pad[self.op.Outputs(0)] = pad
+
+        if pad['channel'] != [0, 0]:
+            self.model.errorMsg.append('Error: Operator PAD: Caffe do not support pad on Channel. ' + str(self.inputs) + ' -> ' + str(self.outputs))
+            self.model.unsupport.append(self.operator_code)
 
 
     def convert(self):
