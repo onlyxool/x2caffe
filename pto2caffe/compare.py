ab8327eaf9687d6502b6ea766ab4d283cdc2ec7e
Kyle <chenkun@vimicro.com>
2022-08-23 20:41:33 +0800

    Update Compare Mechanism

--- pto2caffe/compare.py
+++ pto2caffe/compare.py
@@ -113,4 +113,53 @@ def compare(platform, target_model, caffe_model_path, input_tensor, level=-1):
         print('  cmax_err: %8f'% max_err)
         print('  max_ratio: %8f'% max_ratio, '\n')
 
-#    return (cosin_simi, max_err, max_ratio)
+
+def dump_caffe_model2(caffe_net, input_tensor, level):
+    for i, input_name in enumerate(caffe_net.inputs):
+        caffe_net.blobs[input_name].data[...] = input_tensor
+
+    caffe_output_dict = dict()
+    blob2layer_map = dict()
+
+    if level == 1:
+        caffe_net._forward(0, len(caffe_net.layers)-1)
+        for idx in caffe_net._top_ids(len(caffe_net.layers)-1):
+            data = caffe_net._blobs[idx].data
+            caffe_output_dict[caffe_net._blob_names[idx]] = data.reshape(-1)
+            blob2layer_map[caffe_net._blob_names[idx]] = caffe_net._layer_names[len(caffe_net.layers)-1]
+    elif level == 2:
+        for i in range(len(caffe_net.layers)):
+            caffe_net._forward(i, i)
+            for idx in caffe_net._top_ids(i):
+                data = caffe_net._blobs[idx].data
+                caffe_output_dict[caffe_net._blob_names[idx]] = data.reshape(-1)
+                blob2layer_map[caffe_net._blob_names[idx]] = caffe_net._layer_names[i]
+
+    return caffe_output_dict, blob2layer_map
+
+
+def compare2(model, caffe_net, input_tensor, level=-1):
+
+    caffe_output_dict, blob2layer_map = dump_caffe_model2(caffe_net, input_tensor, level)
+
+    for blob_name in caffe_output_dict:
+        target_output = model.forward(blob_name, input_tensor)
+
+        if target_output is None:
+            continue
+        else:
+            target_output = target_output.reshape(-1)
+
+        print(blob2layer_map[blob_name], '[', blob_name, ']:')
+
+        size_diff = calc_size(caffe_output_dict[blob_name], target_output)
+        if size_diff is not None:
+            print('  Size compare error:', caffe_output_dict[blob_name].shape, target_output.shape)
+
+        cosin_simi = calc_cosine_simi(caffe_output_dict[blob_name], target_output)
+        max_err = calc_max_diff(caffe_output_dict[blob_name], target_output)
+        max_ratio = calc_max_ratio(caffe_output_dict[blob_name], target_output)
+
+        print('  cosin_simi: %8f'% cosin_simi)
+        print('  cmax_err: %8f'% max_err)
+        print('  max_ratio: %8f'% max_ratio, '\n')
