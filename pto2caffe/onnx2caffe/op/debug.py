70a971f04c62ce15d6c5f27b45a208d194ba51be
Kyle <chenkun@vimicro.com>
2022-08-26 16:36:33 +0800

    ONNX2Caffe: Add Debug

---
+++ pto2caffe/onnx2caffe/op/debug.py
@@ -0,0 +1,30 @@
+from caffe_transform import caffe_layer
+from onnx2caffe.op.operator import Operator
+
+
+class Debug(Operator):
+
+    def __init__(self, model, node, index):
+        super().__init__(model, node, index)
+        self.setInited()
+
+
+    def parse(self):
+        self.layer_type = 'Debug'
+        super().__parse__()
+        print('====================')
+        print('Op:', self.name, self.operator_code)
+        print('Input:', self.inputs, self.inputs_shape)
+        print('Output:', self.outputs, self.outputs_shape)
+        print('Attrs:', self.attrs)
+        print('Buf', self.inputs_buf)
+        print('--------------\n')
+        self.setParsed()
+
+
+    def convert(self):
+        layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, debug_param=self.debug_param)
+
+        self.setConverted()
+
+        return [layer]
