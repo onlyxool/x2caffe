b388e91e20b2d1db3221e9c31907973a1101276a
Kyle <chenkun@vimicro.com>
2022-09-01 21:24:54 +0800

    TFLit2Caffe: Add Op Shape

---
+++ pto2caffe/tflite2caffe/op/shape.py
@@ -0,0 +1,21 @@
+from tflite2caffe.op.operator import Operator
+
+
+class Shape(Operator):
+
+    def __init__(self, model, tf_op, tf_op_name, index):
+        super().__init__(model, tf_op, tf_op_name, index)
+        assert(self.operator_code == 'SHAPE')
+        self.setInited()
+
+
+    def parse(self):
+        self.layer_type = 'Shape'
+
+        self.parseInputOutput()
+
+        self.saveConstant(self.outputs[0], self.graph.Tensors(self.op.Inputs(0)).ShapeAsNumpy())
+
+
+    def convert(self):
+        pass
