b6ee6d8eb051044c3eabd018cf9a323e33dba774
Kyle <chenkun@vimicro.com>
2022-09-03 19:48:31 +0800

    ONNX2Caffe: Add Op Cast

---
+++ pto2caffe/onnx2caffe/op/cast.py
@@ -0,0 +1,23 @@
+from onnx2caffe.op.operator import Operator
+
+
+class Cast(Operator):
+
+    def __init__(self, model, node, index):
+        super().__init__(model, node, index)
+        assert(self.operator_code == 'Cast')
+        self.setInited()
+
+
+    def parse(self):
+        self.layer_type = 'Cast'
+        super().__parse__()
+
+        if self.inputs_buf[0] is not None:
+            self.saveConstant(self.node.output[0], self.inputs_buf[0])
+        else:
+            self.byPassOperator()
+
+
+    def convert(self):
+        pass
