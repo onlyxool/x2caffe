294ef6d48d3edfd2cd1b34fbd495a5d9ce530664
Kyle <chenkun@vimicro.com>
2022-09-05 20:56:52 +0800

    Tensorflow2Caffe: Fix Bug in Enter

--- pto2caffe/tensorflow2caffe/op/enter.py
+++ pto2caffe/tensorflow2caffe/op/enter.py
@@ -13,7 +13,7 @@ class Enter(Operator):
         self.layer_type = 'Enter'
         super().__parse__()
 
-        if self.attrs['is_constant']:
+        if self.attrs['is_constant'] or self.inputs_buf[0] is not None:
             self.model.constant[self.outputs[0]] = self.inputs_buf[0]
         else:
             self.model.indentity[self.outputs[0]] = self.model.indentity.get(self.inputs[0], self.inputs[0])
