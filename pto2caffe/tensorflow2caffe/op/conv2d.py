3536a063966eb900ba74f7e34b84fb4f995f688a
Kyle <chenkun@vimicro.com>
2022-09-20 10:53:24 +0800

    Tensorflow2Caffe: Fix Bug in Conv2D & DepthwiseConv2dNative

--- pto2caffe/tensorflow2caffe/op/conv2d.py
+++ pto2caffe/tensorflow2caffe/op/conv2d.py
@@ -15,24 +15,19 @@ class Convolution(Operator):
         self.layer_type = 'Convolution'
         super().__parse__()
 
-        # Weight HWIO -> OIHW
+        # Weight & Bias
         self.weight = self.inputs_buf[1].transpose(3, 2, 0, 1)
         self.inputs_buf[1] = self.weight
-
-        # Bias
-        if len(self.inputs) >= 3:
-            self.bias = self.inputs_buf[2]
-            self.inputs_buf[2] = self.bias
-        else:
-            self.bias = None
+        self.inputs_shape[1] = list(self.weight.shape)
+        self.bias = self.inputs_buf[2] if len(self.inputs) >= 3 else None
 
         # Attribute
         self.convolution_param = dict()
-        self.convolution_param['num_output'] = self.outputs_shape[0][1]
+        self.convolution_param['num_output'] = self.outputs_shape[0][1] if self.outputs_shape[0] is not None else self.weight.shape[0]
         self.convolution_param['stride_h'] = self.attrs['strides'][self.ndim('H')]
         self.convolution_param['stride_w'] = self.attrs['strides'][self.ndim('W')]
         self.convolution_param['dilation'] = [self.attrs['dilations'][self.ndim('H')], self.attrs['dilations'][self.ndim('W')]]
-        self.convolution_param['group'] = int(self.inputs_shape[0][1] / self.weight.shape[1])
+        self.convolution_param['group'] = int(self.inputs_shape[0][1] / self.weight.shape[1]) if self.inputs_shape[0] is not None else 1
         self.convolution_param['kernel_h'] = self.weight.shape[2]
         self.convolution_param['kernel_w'] = self.weight.shape[3]
         self.convolution_param['bias_term'] = True if self.bias is not None else False
--- pto2caffe/tensorflow2caffe/op/depthwise.py
+++ pto2caffe/tensorflow2caffe/op/depthwise.py
@@ -15,26 +15,19 @@ class ConvolutionDepthwise(Operator):
         self.layer_type = 'ConvolutionDepthwise'
         super().__parse__()
 
-        # Weight HWIO -> OIHW
+        # Weight & Bias
         self.weight = self.inputs_buf[1].transpose(2, 3, 0, 1)
-
         self.inputs_buf[1] = self.weight
-        self.inputs_shape[1] = self.weight.shape
-
-        # Bias
-        if len(self.inputs) >= 3:
-            self.bias = self.inputs_buf[2]
-            self.inputs_buf[2] = self.bias
-        else:
-            self.bias = None
+        self.inputs_shape[1] = list(self.weight.shape)
+        self.bias = self.inputs_buf[2] if len(self.inputs) >= 3 else None
 
         # Attribute
         self.convolution_param = dict()
-        self.convolution_param['num_output'] = self.outputs_shape[0][1]
+        self.convolution_param['num_output'] = self.outputs_shape[0][1] if self.outputs_shape[0] is not None else self.weight.shape[0]
         self.convolution_param['stride_h'] = self.attrs['strides'][self.ndim('H')]
         self.convolution_param['stride_w'] = self.attrs['strides'][self.ndim('W')]
         self.convolution_param['dilation'] = [self.attrs['dilations'][self.ndim('H')], self.attrs['dilations'][self.ndim('W')]]
-        self.convolution_param['group'] = int(self.inputs_shape[0][1] / self.weight.shape[1])
+        self.convolution_param['group'] = self.inputs_shape[0][1]#int(self.inputs_shape[0][1] / self.weight.shape[1]) if self.inputs_shape[0] is not None else 1
         self.convolution_param['kernel_h'] = self.weight.shape[2]
         self.convolution_param['kernel_w'] = self.weight.shape[3]
         self.convolution_param['bias_term'] = True if self.bias is not None else False
