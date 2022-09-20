2d86fafad9b7880f541ce4d1fbfad6b42377c986
Kyle <chenkun@vimicro.com>
2022-09-20 10:42:32 +0800

    Tensorflow2Caffe: Maximum -> Eltwise

--- pto2caffe/tensorflow2caffe/op/maximum.py
+++ pto2caffe/tensorflow2caffe/op/maximum.py
@@ -28,25 +28,34 @@ class Maximum(Operator):
             self.attrs = self.eltwise_param
             self.setParsed()
         elif self.inputs_buf[1] is not None:
-            self.layer_type = 'ReLU'
-
-            # Check weather y == 0
             if np.count_nonzero(self.inputs_buf[1]) > 0:
-                self.unSupported('Does not Support y != 0.')
-                return
+                self.layer_type = 'Dummy+Eltwise'
+                self.dummy_data_param = dict()
+                self.dummy_data_param['data_filler'] = dict(type='constant', value=self.inputs_buf[1])
+                self.dummy_data_param['shape'] = dict(dim=self.inputs_shape[0])
 
-            # Attribute
-            self.relu_param = dict()
-            self.relu_param['negative_slope'] = 0
+                self.inter_blob = 'preDummy_split' + str(self.index)
 
-            self.attrs = self.relu_param
+                self.eltwise_param = dict()
+                self.eltwise_param['operation'] = 2
 
-            self.setParsed()
+                self.attrs = self.eltwise_param
+                self.setParsed()
+            else:
+                self.layer_type = 'ReLU'
+                self.relu_param = dict()
+                self.relu_param['negative_slope'] = 0
+
+                self.attrs = self.relu_param
+                self.setParsed()
 
 
     def convert(self):
         if self.type == 'Eltwise':
             layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, eltwise_param=self.eltwise_param)
+        elif self.type == 'Dummy+Eltwise':
+            layers.append(caffe_layer('Dummy', 'Dummy'+str(self.index), self.inputs, self.inputs_buf, [self.inter_blob], dummy_data_param=self.dummy_data_param))
+            layers.append(caffe_layer('Eltwise', 'Eltwise'+str(self.index), [self.inter_blob], self.inputs_buf, self.outputs, eltwise_param=self.eltwise_param))
         elif self.type == 'ReLU':
             layer = caffe_layer(self.type, self.name, self.inputs, self.inputs_buf, self.outputs, relu_param=self.relu_param)
 
