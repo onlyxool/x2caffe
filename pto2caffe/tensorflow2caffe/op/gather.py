c9d69d345429ffb9e9183a6d3659916d896c9fa6
Kyle <chenkun@vimicro.com>
2022-09-20 10:36:02 +0800

    Tensorflow2Caffe: Upadate Op Gather

--- pto2caffe/tensorflow2caffe/op/gather.py
+++ pto2caffe/tensorflow2caffe/op/gather.py
@@ -15,11 +15,15 @@ class GatherV2(Operator):
         self.layer_type = 'GatherV2'
         super().__parse__()
 
-        if self.inputs_buf[1] is not None:
+        if self.inputs_buf[0] is not None and self.inputs_buf[1] is not None and self.inputs_buf[2] is not None:
             params = tf.constant(self.inputs_buf[0], dtype=self.op.inputs[0].dtype)
             indices = tf.constant(self.inputs_buf[1], dtype=self.op.inputs[1].dtype)
             axis = tf.constant(self.inputs_buf[2], dtype=self.op.inputs[2].dtype)
             self.saveConstant(self.outputs[0], tf.raw_ops.GatherV2(params=params, indices=indices, axis=axis, batch_dims=self.attrs['batch_dims'], name=None).numpy())
+        elif self.inputs_buf[0] is None:
+            self.unSupported('Cant Parse param is None')
+        elif self.inputs_buf[2] is None:
+            self.unSupported('Cant Parse axis is None')
         else:
             self.unSupported()
 
