d6b8dc8040884c3cdedaf29d693439d9602edd8c
Kyle <chenkun@vimicro.com>
2022-09-20 10:34:28 +0800

    Tensorflow2Caffe: Fix Bug in RealDiv

--- pto2caffe/tensorflow2caffe/op/realdiv.py
+++ pto2caffe/tensorflow2caffe/op/realdiv.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from caffe_transform import caffe_layer
 from tensorflow2caffe.op.operator import Operator
 
@@ -17,20 +19,12 @@ class RealDiv(Operator):
 
         if self.inputs_buf[0] is not None and self.inputs_buf[1] is not None:
             self.saveConstant(self.outputs[0], self.inputs_buf[0] / self.inputs_buf[1])
-        elif self.inputs_buf[1] is not None or self.inputs_buf[0] is not None:
+        elif self.inputs_buf[0] is None and self.inputs_buf[1] is not None:
             self.layer_type = 'Scale'
-
-            if self.inputs_buf[0] is not None:
-                self.inputs.reverse()
-                self.inputs_shape.reverse()
-                self.inputs_buf.reverse()
 
             # Scale Parameter
             self.scale_param = dict()
-            if len(self.inputs_shape[1]) > 0:
-                self.scale_param['axis'] = self.inputs_shape[0].index(self.inputs_shape[1][0])
-            else:
-                self.scale_param['axis'] = 0
+            self.scale_param['axis'] = self.inputs_shape[0].index(self.inputs_shape[1][0]) if np.ones(self.inputs_shape[1]).size > 1 else 0
             self.scale_param['num_axes'] = len(self.inputs_shape[1])
             self.scale_param['bias_term'] = False
 
@@ -46,6 +40,7 @@ class RealDiv(Operator):
             self.bias = None
 
             self.attrs = self.scale_param
+
             self.setParsed()
         else:
             self.unSupported()
