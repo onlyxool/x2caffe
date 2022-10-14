77f9258c050c6b485c2510b2551cba3c5eab9d3e
Kyle <chenkun@vimicro.com>
2022-10-14 18:10:41 +0800

    Tvm2Caffe: Add Op BatchNorm

---
+++ pto2caffe/tvm2caffe/op/batchnorm.py
@@ -0,0 +1,47 @@
+from caffe_transform import caffe_layer
+from tvm2caffe.op.operator import Operator
+
+
+class BatchNorm(Operator):
+
+    def __init__(self, model, relay, index):
+        super().__init__(model, relay, index)
+        assert(self.operator_code == 'nn.batch_norm')
+        self.setInited()
+
+
+    def parse(self):
+        self.type = 'BatchNorm+Scale'
+        super().__parse__()
+
+        # Weight
+        self.weight = self.inputs_buf[1]
+
+        # Bias
+        self.bias = self.inputs_buf[2]
+
+        # Mean
+        self.mean = self.inputs_buf[3]
+
+        # var
+        self.var = self.inputs_buf[4]
+
+        self.batch_norm_param = dict()
+        self.batch_norm_param['eps'] = self.attrs.get('epsilon', 1e-5)
+        self.batch_norm_param['use_global_stats'] = True
+
+        self.scale_param = dict()
+        self.scale_param['bias_term'] = True
+
+        self.attrs = self.batch_norm_param
+
+        self.setParsed()
+
+
+    def convert(self):
+        layer0 = caffe_layer(self.layer_type[0], self.name[0], self.inputs, self.inputs_buf, self.outputs, self.mean, self.var, batch_norm_param=self.batch_norm_param)
+        layer1 = caffe_layer(self.layer_type[1], self.name[1], self.outputs, self.inputs_buf, self.outputs, self.weight, self.bias, scale_param=self.scale_param)
+
+        self.setConverted()
+
+        return [layer0, layer1]
