c15992bb887f44af0083f456c98779b88b64bb04
Kyle <chenkun@vimicro.com>
2022-10-14 14:47:14 +0800

    ONNX2Caffe: Fix Bug in Gather

--- pto2caffe/onnx2caffe/op/gather.py
+++ pto2caffe/onnx2caffe/op/gather.py
@@ -1,3 +1,4 @@
+from caffe_transform import caffe_layer
 from onnx2caffe.op.operator import Operator
 
 
@@ -12,15 +13,15 @@ class Gather(Operator):
     def parse(self):
         super().__parse__()
 
-        if self.inputs_buf[0] is not None and self.inputs_buf[0].size > 0:
+        if self.inputs_buf[0] is not None and self.inputs_buf[1] is not None and self.inputs_buf[1].size > 0:
             import numpy as np
-            self.saveConstant(self.node.output[0], np.take(self.inputs_buf[0], indices=self.inputs_buf[1], axis=self.attrs['axis']))
-        elif self.inputs_buf[1] is not None and self.inputs_buf[1].size == 1:
+            self.saveConstant(self.node.output[0], np.take(self.inputs_buf[0], indices=self.inputs_buf[1], axis=self.attrs.get('axis', 0)))
+        elif self.inputs_buf[1] is not None and self.inputs_buf[1].size == 1: #TODO
             self.type = 'Slice'
 
             self.slice_param = dict()
             self.slice_param['axis'] = self.attrs['axis']
-            self.slice_param['slice_point'] = self.inputs_shape[0][self.attrs['axis']]
+            self.slice_param['slice_point'] = 1#self.inputs_shape[0][self.attrs['axis']]
             self.attrs = self.slice_param
             self.setParsed()
         else:
