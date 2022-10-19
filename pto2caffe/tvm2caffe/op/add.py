44143bb0ceef517ad01ffd44d99fee4571b6f5cb
Kyle <chenkun@vimicro.com>
2022-10-19 17:23:15 +0800

    Tvm2Caffe: add -> BiasAdd

--- pto2caffe/tvm2caffe/op/add.py
+++ pto2caffe/tvm2caffe/op/add.py
@@ -1,5 +1,8 @@
+import numpy as np
 from caffe_transform import caffe_layer
 from tvm2caffe.op.operator import Operator
+
+from util import dim_map_nhwc2nchw
 
 
 class Add(Operator):
@@ -13,15 +16,39 @@ class Add(Operator):
     def parse(self):
         super().__parse__()
 
-        self.type = 'Eltwise'
-        self.eltwise_param = dict()
-        self.eltwise_param['operation'] = 1 # Caffe Eltwise SUM
-        self.attrs = self.eltwise_param
-        self.setParsed()
+        if self.inputs_buf[0] is None and self.inputs_buf[1] is None and self.inputs_shape[0] == self.inputs_shape[1]:
+            self.type = 'Eltwise'
+            self.eltwise_param = dict()
+            self.eltwise_param['operation'] = 1 # Caffe Eltwise SUM
+            self.attrs = self.eltwise_param
+            self.setParsed()
+        elif (self.inputs_buf[0] is not None or self.inputs_buf[1] is not None) or (self.inputs_shape[0] != self.inputs_shape[1]):
+            self.type = 'Bias'
+
+            if self.inputs_buf[1] is not None and np.all(self.inputs_buf[1] == 1):
+                self.byPassOperator()
+                return
+
+            self.bias_param = dict()
+            if 'axis' in self.attrs:
+                self.bias_param['axis'] = self.attrs['axis']
+            else:
+                self.bias_param['axis'] = self.inputs_shape[0].index(self.inputs_shape[1][0]) if len(self.inputs_shape[1]) > 0 else 0
+
+            self.bias_param['axis'] = dim_map_nhwc2nchw[self.bias_param['axis']] if self.layout == 'NHWC' and len(self.inputs_shape[0]) == 4 else self.bias_param['axis']
+            self.bias_param['num_axes'] = len(self.inputs_shape[1])
+            self.attrs = self.bias_param
+
+            self.bias = self.inputs_buf[1]
+
+            self.setParsed()
 
 
     def convert(self):
-        layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, eltwise_param=self.eltwise_param)
+        if self.type == 'Eltwise':
+            layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, eltwise_param=self.eltwise_param)
+        elif self.type == 'Bias':
+            layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, self.bias, bias_param=self.bias_param)
 
         self.setConverted()
 
