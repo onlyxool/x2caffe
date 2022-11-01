0e04ab2195da4757d53ff7d0dd69aa850660742b
Kyle <chenkun@vimicro.com>
2022-11-01 16:54:14 +0800

    Tvm2Caffe: Add Op power

---
+++ pto2caffe/tvm2caffe/op/power.py
@@ -0,0 +1,32 @@
+from caffe_transform import caffe_layer
+from tvm2caffe.op.operator import Operator
+
+
+class Power(Operator):
+
+    def __init__(self, model, relay, index):
+        super().__init__(model, relay, index)
+        assert(self.operator_code == 'power')
+        self.setInited()
+
+
+    def parse(self):
+        self.type = 'Power'
+        super().__parse__()
+
+        self.power_param = dict()
+        self.power_param['power'] = self.inputs_buf[1]
+        self.power_param['scale'] = 1
+        self.power_param['shift'] = 0
+
+        self.attrs = self.power_param
+
+        self.setParsed()
+
+
+    def convert(self):
+        layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, power_param=self.power_param)
+
+        self.setConverted()
+
+        return [layer]
