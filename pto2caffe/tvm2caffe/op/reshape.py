4579b172fef85825e85949bb1af537606642d9e0
Kyle <chenkun@vimicro.com>
2022-11-03 20:07:43 +0800

    Tvm2Caffe: Add Op squeeze

--- pto2caffe/tvm2caffe/op/reshape.py
+++ pto2caffe/tvm2caffe/op/reshape.py
@@ -6,7 +6,7 @@ class Reshape(Operator):
 
     def __init__(self, model, relay, index):
         super().__init__(model, relay, index)
-        assert(self.operator_code == 'reshape')
+        assert(self.operator_code in ('reshape', 'squeeze'))
         self.setInited()
 
 
@@ -17,9 +17,14 @@ class Reshape(Operator):
             self.saveConstant(self.outputs[0], self.inputs_buf[0].reshape(self.attrs['newshape']))
         elif self.inputs_shape[0] is not None and self.outputs_shape[0] is not None and self.inputs_shape[0] == self.outputs_shape[0]:
             self.byPassOperator()
-        else:
+        elif self.operator_code == 'reshape':
             self.type = 'Reshape'
             self.reshape_param = dict(shape=dict(dim=self.attrs['newshape']))
+            self.attrs = self.reshape_param
+            self.setParsed()
+        elif self.operator_code == 'squeeze':
+            self.type = 'Reshape'
+            self.reshape_param = dict(shape=dict(dim=self.outputs_shape[0]))
             self.attrs = self.reshape_param
             self.setParsed()
 
