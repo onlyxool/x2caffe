9971f8c2d91cdd1e4f51e7b65f493cd988de018e
Kyle <chenkun@vimicro.com>
2022-11-03 15:34:05 +0800

    Tvm2Caffe: Fix Bug get axis in Concat when layout == NHWC

--- pto2caffe/tvm2caffe/op/concat.py
+++ pto2caffe/tvm2caffe/op/concat.py
@@ -1,6 +1,7 @@
 from caffe_transform import caffe_layer
 from tvm2caffe.op.operator import Operator
 
+from util import dim_map_nhwc2nchw
 
 class Concat(Operator):
     def __init__(self, model, relay, index):
@@ -14,7 +15,7 @@ class Concat(Operator):
         self.type = 'Concat'
 
         self.concat_param = dict()
-        self.concat_param['axis'] = self.attrs['axis']
+        self.concat_param['axis'] = dim_map_nhwc2nchw[self.attrs['axis']] if self.layout == 'NHWC' else self.attrs['axis']
 
         self.attrs = self.concat_param
 
