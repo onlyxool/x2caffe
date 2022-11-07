2889d672e0a1cc07c1aeb1baadacaca9ec5a07d6
Kyle <chenkun@vimicro.com>
2022-11-07 18:24:44 +0800

    Tvm2Caffe: Add Op cast

--- pto2caffe/tvm2caffe/op/bypassoperator.py
+++ pto2caffe/tvm2caffe/op/bypassoperator.py
@@ -5,17 +5,14 @@ class ByPassOperator(Operator):
 
     def __init__(self, model, relay, index):
         super().__init__(model, relay, index)
-        assert(self.operator_code in ('bypass', 'array'))
+        assert(self.operator_code in ('bypass', 'array', 'cast'))
         self.setInited()
 
 
     def parse(self):
         super().__parse__()
 
-        if self.operator_code == 'bypass':
-            self.byPassOperator()
-        elif self.operator_code == 'array':
-            self.byPassOperator()
+        self.byPassOperator()
 
 
     def convert(self):
