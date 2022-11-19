a75e81b1c7b1893cedc3bd495438b2c6380f3b58
Kyle <chenkun@vimicro.com>
2022-11-19 18:06:05 +0800

    Tvm2Caffe: Add OP exp

---
+++ pto2caffe/tvm2caffe/op/exp.py
@@ -0,0 +1,34 @@
+from caffe_transform import caffe_layer
+from tvm2caffe.op.operator import Operator
+
+
+class Exp(Operator):
+
+    def __init__(self, model, relay, index):
+        super().__init__(model, relay, index)
+        assert(self.operator_code == 'exp')
+        self.setInited()
+
+
+    def parse(self):
+        self.type = 'Exp'
+        super().__parse__()
+
+        # Attributes
+        # Leave all arguments in ExpParameter as default
+        # base = -1.0 (base = e)
+        # scale = 1.0
+        # shift = 0.0
+
+        self.exp_param = dict()
+        self.attrs = self.exp_param
+
+        self.setParsed()
+
+
+    def convert(self):
+        layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, exp_param=self.exp_param)
+
+        self.setConverted()
+
+        return [layer]
