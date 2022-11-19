5ff5232305bc39637d991ba2a07d8e275d95db81
Kyle <chenkun@vimicro.com>
2022-11-19 18:06:28 +0800

    Tvm2Caffe: Add OP log

---
+++ pto2caffe/tvm2caffe/op/log.py
@@ -0,0 +1,35 @@
+from caffe_transform import caffe_layer
+from tvm2caffe.op.operator import Operator
+
+
+class Log(Operator):
+
+    def __init__(self, model, relay, index):
+        super().__init__(model, relay, index)
+        assert(self.operator_code == 'log')
+        self.setInited()
+
+
+    def parse(self):
+        self.type = 'Log'
+        super().__parse__()
+
+        # Attributes
+        # Leave all arguments in LogParameter as default
+        # base = -1.0 (base = e)
+        # scale = 1.0
+        # shift = 0.0
+
+        self.log_param = dict()
+        self.attrs = self.log_param
+
+        self.setParsed()
+
+
+    def convert(self):
+        layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, log_param=self.log_param)
+
+        self.setConverted()
+
+        return [layer]
+
