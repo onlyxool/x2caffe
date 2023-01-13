e00e705371137b557dd4b615f71e661c77ca39f8
Kyle <chenkun@vimicro.com>
2023-01-13 10:46:02 +0800

    ONNX2Caffe: Convert Clip to ReLU when max value is None

--- pto2caffe/onnx2caffe/op/clip.py
+++ pto2caffe/onnx2caffe/op/clip.py
@@ -11,22 +11,34 @@ class ReLUX(Operator):
 
 
     def parse(self):
-        self.type = 'ReLUX'
         super().__parse__()
 
-        # Attributes
-        self.relux_param = dict()
-        if 'max' in self.attrs and 'min' in self.attrs:
-            self.relux_param['x'] = self.attrs['max']
+        if self.inputs_buf[2] is not None:
+            self.type = 'ReLUX'
+
+            self.relux_param = dict()
+            if 'max' in self.attrs and 'min' in self.attrs:
+                self.relux_param['x'] = self.attrs['max']
+            else:
+                self.relux_param['x'] = self.inputs_buf[2]
+            self.attrs = self.relux_param
         else:
-            self.relux_param['x'] = self.inputs_buf[2] if self.inputs_buf[2] is not None else 6
-        self.attrs = self.relux_param
+            self.type = 'ReLU'
+
+            self.relu_param = dict()
+            self.relu_param['negative_slope'] = self.attrs.get('alpha', 0)
+            self.attrs = self.relu_param
+
+        self.inputs = self.inputs[:1]
 
         self.setParsed()
 
 
     def convert(self):
-        layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, relux_param=self.relux_param)
+        if self.type == 'ReLUX':
+            layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, relux_param=self.relux_param)
+        elif self.type == 'ReLU':
+            layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, relu_param=self.relu_param)
 
         self.setConverted()
 
