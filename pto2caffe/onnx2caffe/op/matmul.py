d7d7597feb8d74a93a1d58e1a279abb1b8b0dfee
Kyle <chenkun@vimicro.com>
2023-01-13 11:10:22 +0800

    ONNX2Caffe: MatMul -> MatMul

--- pto2caffe/onnx2caffe/op/matmul.py
+++ pto2caffe/onnx2caffe/op/matmul.py
@@ -16,14 +16,7 @@ class MatMul(Operator):
         if self.inputs_buf[0] is not None and self.inputs_buf[0] is not None:
             import numpy as np
             self.saveConstant(self.outputs[0], np.matmul(self.inputs_buf[0], self.inputs_buf[1]))
-        elif self.inputs_buf[0] is None and self.inputs_buf[1] is None:
-            self.unSupported()
-            return
-        else:
-            if len(self.inputs_shape[0]) != 2 or len(self.inputs_shape[1]) != 2:
-                self.unSupported('only support input dimentions == 2')
-                return
-
+        elif self.inputs_buf[0] is None and self.inputs_buf[1] is not None and (len(self.inputs_shape[0]) == 2 and len(self.inputs_shape[1]) == 2):
             self.type = 'InnerProduct'
             # Weight
             self.weight = self.inputs_buf[1].transpose(1,0)
@@ -43,15 +36,21 @@ class MatMul(Operator):
             self.attrs = self.inner_product_param
 
             self.setParsed()
+        else:
+            self.type = 'MatMul'
+            self.matmul_param = dict()
+            self.matmul_param['transpose_a'] = 0
+            self.matmul_param['transpose_b'] = 0
+            self.attrs = self.matmul_param
+            self.setParsed()
 
 
     def convert(self):
-        layers = list()
         if self.type == 'InnerProduct':
-            layers.append(caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, self.weight, self.bias, inner_product_param=self.inner_product_param))
+            layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, self.weight, self.bias, inner_product_param=self.inner_product_param)
         elif self.type == 'MatMul':
-            layers.append(caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, matmul_param=self.matmul_param))
+            layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, matmul_param=self.matmul_param)
 
         self.setConverted()
 
-        return layers
+        return [layer]
