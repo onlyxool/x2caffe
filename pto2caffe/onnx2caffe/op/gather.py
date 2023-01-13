3fb78a024c82d31b27f8a5ff9cab44eaf8896917
Kyle <chenkun@vimicro.com>
2023-01-13 11:08:01 +0800

    ONNX2Caffe: Convert Gather to Reshape when indices == 0

--- pto2caffe/onnx2caffe/op/gather.py
+++ pto2caffe/onnx2caffe/op/gather.py
@@ -16,6 +16,12 @@ class Gather(Operator):
         if self.inputs_buf[0] is not None and self.inputs_buf[1] is not None and self.inputs_buf[1].size > 0:
             import numpy as np
             self.saveConstant(self.node.output[0], np.take(self.inputs_buf[0], indices=self.inputs_buf[1], axis=self.attrs.get('axis', 0)))
+        elif self.inputs_buf[1] is not None and self.inputs_buf[1] == 0:
+            self.type = 'Reshape'
+
+            self.reshape_param = dict(shape=dict(dim=self.outputs_shape[0]))
+            self.attrs = self.reshape_param
+            self.setParsed()
         elif self.inputs_buf[1] is not None and self.inputs_buf[1].size == 1: #TODO
             self.type = 'Slice'
 
@@ -29,7 +35,10 @@ class Gather(Operator):
 
 
     def convert(self):
-        layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, slice_param=self.slice_param)
+        if self.type == 'Reshape':
+            layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, reshape_param=self.reshape_param)
+        elif self.type == 'Slice':
+            layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, slice_param=self.slice_param)
 
         self.setConverted()
 
