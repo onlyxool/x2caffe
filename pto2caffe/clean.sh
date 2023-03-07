fbc19fc6fca366a334ac84ab250f35ececd3a51a
Kyle <chenkun@vimicro.com>
2023-03-07 18:42:52 +0800

    Remove test.py Update clean.sh

--- pto2caffe/clean.sh
+++ pto2caffe/clean.sh
@@ -1,5 +1,5 @@
 # Remove all pyc and __pycache__
 find . -name '*.pyc' -type f -print -exec rm {} \;
 find . -name '__pycache__' -exec rmdir {} \;
-rm dbg_log.txt
+rm .dbg_log.txt
 rm pytorch2caffe/libpnnx.so
