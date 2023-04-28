1752b836b7bf27fc07c6fbe500e9ac35faf75b24
Kyle <chenkun@vimicro.com>
2023-04-28 17:32:44 +0800

    ONNX2Caffe: Add Shape Compatible Check in Sub

--- pto2caffe/onnx2caffe/op/sub.py
+++ pto2caffe/onnx2caffe/op/sub.py
@@ -45,6 +45,15 @@ class Sub(Operator):
             self.scale_param['num_axes'] = len(self.inputs_shape[1])
             self.scale_param['bias_term'] = False
 
+            BiasShape = self.inputs_shape[1]
+            CompatibleFlag = self.checkShapeCompatible()
+            if CompatibleFlag == 'Squeeze':
+                self.type = 'Scale+Reshape+Bias'
+                self.reshape_param = dict(shape=dict(dim=self.inputs_shape[1]))
+            elif not CompatibleFlag:
+                self.unSupported('Inputs incompatible shape for Caffe. ' + str(self.inputs_shape[0]) + ' - ' + str(BiasShape))
+                return
+
             self.bias_param = dict()
             self.bias_param['axis'] = self.inputs_shape[0].index(self.inputs_shape[1][0]) if len(self.inputs_shape[1]) > 0 else 0
             self.bias_param['num_axes'] = len(self.inputs_shape[1])
@@ -83,6 +92,10 @@ class Sub(Operator):
         elif self.type == 'Scale+Bias':
             layers.append(caffe_layer(self.layer_type[0], self.name[0], [self.inputs[1]], self.inputs_buf, self.interblob, self.weight, scale_param=self.scale_param))
             layers.append(caffe_layer(self.layer_type[1], self.name[1], [self.inputs[0], self.interblob[0]], self.inputs_buf, self.outputs, self.bias, bias_param=self.bias_param))
+        elif self.type == 'Scale+Reshape+Bias':
+            layers.append(caffe_layer(self.layer_type[0], self.name[0], [self.inputs[1]], self.inputs_buf, [self.interblob[0]], self.weight, scale_param=self.scale_param))
+            layers.append(caffe_layer(self.layer_type[1], self.name[1], [self.interblob[0]], [None], [self.interblob[1]], reshape_param=self.reshape_param))
+            layers.append(caffe_layer(self.layer_type[2], self.name[2], [self.inputs[0], self.interblob[1]], self.inputs_buf, self.outputs, self.bias, bias_param=self.bias_param))
         elif self.type == 'Bias+Scale':
             layers.append(caffe_layer(self.layer_type[0], self.name[0], self.inputs, self.inputs_buf, self.interblob, self.bias, bias_param=self.bias_param))
             layers.append(caffe_layer(self.layer_type[1], self.name[1], self.interblob, [None, self.weight], self.outputs, self.weight, scale_param=self.scale_param))
