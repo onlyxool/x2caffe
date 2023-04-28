577745c8891f1afdb29271b7d565fa359672c745
Kyle <chenkun@vimicro.com>
2023-04-28 18:24:53 +0800

    Torch2Caffe: Add OP unsqueeze

---
+++ pto2caffe/torch2caffe/op/unsqueeze.py
@@ -0,0 +1,41 @@
+from caffe_transform import caffe_layer
+from torch2caffe.op.operator import Operator
+
+
+class Unsqueeze(Operator):
+
+    def __init__(self, model, node, index):
+        super().__init__(model, model.graph, node, index)
+        assert(self.operator_code == 'unsqueeze')
+        self.setInited()
+
+
+    def compute_output_shape(self):
+        if not self.isInputShapeFullyDefined(0):
+            self.unSupported('Illegal Input Shape.')
+            return
+
+        self.outputs_shape[0].insert(self.inputs_buf[1], 1)
+        self.model.tensor_shape[self.outputs[0]] = self.outputs_shape[0]
+
+
+    def parse(self):
+        super().__parse__()
+
+        self.type = 'Reshape'
+
+        self.compute_output_shape()
+
+        self.reshape_param = dict(shape=dict(dim=self.outputs_shape[0]))
+
+        self.attrs = self.reshape_param
+
+        self.setParsed()
+
+
+    def convert(self):
+        layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, reshape_param=self.reshape_param)
+
+        self.setConverted()
+
+        return [layer]
