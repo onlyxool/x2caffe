46d600c9b1e364f031bfcc3a4f2967f4094b9f2a
Kyle <chenkun@vimicro.com>
2023-04-28 18:21:37 +0800

    Torch2Caffe: Add OP relu

---
+++ pto2caffe/torch2caffe/op/relu.py
@@ -0,0 +1,30 @@
+from caffe_transform import caffe_layer
+from torch2caffe.op.operator import Operator
+
+
+class ReLU(Operator):
+
+    def __init__(self, model, node, index):
+        super().__init__(model, model.graph, node, index)
+        assert(self.operator_code == 'relu')
+        self.setInited()
+
+
+    def parse(self):
+        self.type = 'ReLU'
+        super().__parse__()
+
+        self.relu_param = dict()
+        self.relu_param['negative_slope'] = 0
+
+        self.attrs = self.relu_param
+
+        self.setParsed()
+
+
+    def convert(self):
+        layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, relu_param=self.relu_param)
+
+        self.setConverted()
+
+        return [layer]
