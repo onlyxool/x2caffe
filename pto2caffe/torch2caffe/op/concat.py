366fccc27da105c54f1803c9377491079a5d59f6
Kyle <chenkun@vimicro.com>
2023-04-28 18:11:24 +0800

    Torch2Caffe: Add Op cat

---
+++ pto2caffe/torch2caffe/op/concat.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+from caffe_transform import caffe_layer
+from torch2caffe.op.operator import Operator
+
+
+class Concat(Operator):
+
+    def __init__(self, model, node, index):
+        super().__init__(model, model.graph, node, index)
+        assert(self.operator_code == 'cat')
+        self.setInited()
+
+
+    def compute_output_shape(self):
+        if not self.isInputShapeFullyDefined(-1):
+            self.unSupported('Illegal Input Shape.' + str(self.inputs_shape))
+            return
+
+        self.outputs_shape[0][self.concat_param['axis']] = np.sum([shape[self.concat_param['axis']] for shape in self.inputs_shape[:-1]])
+        self.model.tensor_shape[self.outputs[0]] = self.outputs_shape[0]
+
+
+    def parse(self):
+        self.type = 'Concat'
+        super().__parse__()
+
+        self.concat_param = dict()
+        self.concat_param['axis'] = self.inputs_buf[-1]
+
+        self.attrs = self.concat_param
+        self.compute_output_shape()
+
+        self.setParsed()
+
+
+    def convert(self):
+        layer = caffe_layer(self.layer_type, self.name, self.inputs[:-1], self.inputs_buf, self.outputs, concat_param=self.concat_param)
+
+        self.setConverted()
+
+        return [layer]
