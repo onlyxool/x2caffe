651b34e555aa83fd9ee320c031b3d9c05e599b18
Kyle <chenkun@vimicro.com>
2023-04-28 18:22:25 +0800

    Torch2Caffe: Add OP sigmoid

---
+++ pto2caffe/torch2caffe/op/sigmoid.py
@@ -0,0 +1,29 @@
+from caffe_transform import caffe_layer
+from torch2caffe.op.operator import Operator
+
+
+class Sigmoid(Operator):
+
+    def __init__(self, model, node, index):
+        super().__init__(model, model.graph, node, index)
+        assert(self.operator_code == 'sigmoid')
+        self.setInited()
+
+
+    def parse(self):
+        self.type = 'Sigmoid'
+        super().__parse__()
+
+        self.sigmoid_param = dict()
+
+        self.attrs = self.sigmoid_param
+
+        self.setParsed()
+
+
+    def convert(self):
+        layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, sigmoid_param=self.sigmoid_param)
+
+        self.setConverted()
+
+        return [layer]
