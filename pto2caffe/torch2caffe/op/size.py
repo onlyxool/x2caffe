b5f955a6b6b98dbcf5bed4144b3e0a48c9f848a3
Kyle <chenkun@vimicro.com>
2023-04-28 18:22:41 +0800

    Torch2Caffe: Add OP size

---
+++ pto2caffe/torch2caffe/op/size.py
@@ -0,0 +1,24 @@
+from torch2caffe.op.operator import Operator
+
+
+class Size(Operator):
+
+    def __init__(self, model, node, index):
+        super().__init__(model, model.graph, node, index)
+        assert(self.operator_code == 'size')
+        self.setInited()
+
+
+    def parse(self):
+        self.type = 'Size'
+        super().__parse__()
+
+        if not self.isInputShapeFullyDefined(0):
+            self.unSupported('Illegal Input Shape.')
+            return
+
+        self.saveConstant(self.outputs[0], self.inputs_shape[0][self.inputs_buf[1]])
+
+
+    def convert(self):
+        pass
