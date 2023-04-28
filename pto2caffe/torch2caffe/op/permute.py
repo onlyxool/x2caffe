9aefc298619c7362bf2aa6e89230d16f6a6275f8
Kyle <chenkun@vimicro.com>
2023-04-28 18:20:42 +0800

    Torch2Caffe: Add OP permute

---
+++ pto2caffe/torch2caffe/op/permute.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+from caffe_transform import caffe_layer
+from torch2caffe.op.operator import Operator
+
+
+class Permute(Operator):
+
+    def __init__(self, model, node, index):
+        super().__init__(model, model.graph, node, index)
+        assert(self.operator_code == 'permute')
+        self.setInited()
+
+
+    def compute_output_shape(self):
+        if not self.isInputShapeFullyDefined(0) or len(self.inputs_shape[0]) != len(self.inputs_buf[1]):
+            self.unSupported('Illegal Input Shape.')
+            return
+
+        self.outputs_shape[0] = list(np.zeros(self.inputs_shape[0]).transpose(self.inputs_buf[1]).shape)
+        self.model.tensor_shape[self.outputs[0]] = self.outputs_shape[0]
+
+
+    def parse(self):
+        super().__parse__()
+
+        self.type = 'Permute'
+        self.permute_param = dict()
+        self.permute_param['order'] = self.inputs_buf[1]
+
+        self.attrs = self.permute_param
+
+        self.compute_output_shape()
+        self.setParsed()
+
+
+    def convert(self):
+        layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, permute_param=self.permute_param)
+
+        self.setConverted()
+
+        return [layer]
