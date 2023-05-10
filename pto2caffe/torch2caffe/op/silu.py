3646930c69aea330fecb5b93b37a4e1e039dea52
Kyle <chenkun@vimicro.com>
2023-05-10 11:50:52 +0800

    Torch2Caffe: Add OP silu

---
+++ pto2caffe/torch2caffe/op/silu.py
@@ -0,0 +1,30 @@
+from caffe_transform import caffe_layer
+from torch2caffe.op.operator import Operator
+
+
+class Silu(Operator):
+
+    def __init__(self, model, node, index):
+        super().__init__(model, model.graph, node, index)
+        assert(self.operator_code == 'silu')
+        self.setInited()
+
+
+    def parse(self):
+        self.type = 'Swish'
+        super().__parse__()
+
+        self.swish_param = dict()
+        self.swish_param['beta'] = 1.0 
+
+        self.attrs = self.swish_param
+
+        self.setParsed()
+
+
+    def convert(self):
+        layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, swish_param=self.swish_param)
+
+        self.setConverted()
+
+        return [layer]
