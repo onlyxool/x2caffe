d145b38e45a5cb9b28d593f533fff26eda346c89
Kyle <chenkun@vimicro.com>
2023-05-10 11:58:49 +0800

    Torch2Caffe: Add Method compute_output_shape in OP flatten

--- pto2caffe/torch2caffe/op/flatten.py
+++ pto2caffe/torch2caffe/op/flatten.py
@@ -1,3 +1,4 @@
+import torch
 from caffe_transform import caffe_layer
 from torch2caffe.op.operator import Operator
 
@@ -10,12 +11,23 @@ class Flatten(Operator):
         self.setInited()
 
 
+    def compute_output_shape(self):
+        if not self.isInputShapeFullyDefined(0):
+            self.unSupported('Illegal Input Shape.' + str(self.inputs_shape))
+            return
+
+        self.outputs_shape[0] = list(torch.flatten(torch.rand(self.outputs_shape[0]), self.inputs_buf[1], self.inputs_buf[2]).shape)
+        self.model.tensor_shape[self.outputs[0]] = self.outputs_shape[0]
+
+
     def parse(self):
         self.type = 'Flatten'
         super().__parse__()
+        self.compute_output_shape()
 
         self.flatten_param = dict()
         self.flatten_param['axis'] = self.inputs_buf[1]
+        self.flatten_param['end_axis'] = self.inputs_buf[2]
         self.attrs = self.flatten_param
 
         self.setParsed()
