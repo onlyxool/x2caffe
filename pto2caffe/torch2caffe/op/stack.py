ad2ff82d6528cb29a67a318e35709d2bf9ad54f5
Kyle <chenkun@vimicro.com>
2023-05-18 18:39:13 +0800

    Torch2Caffe: Add forward in OP stack    Remove compute_output_shape    Fix Bug

--- pto2caffe/torch2caffe/op/stack.py
+++ pto2caffe/torch2caffe/op/stack.py
@@ -1,3 +1,4 @@
+import torch
 import numpy as np
 from copy import deepcopy
 
@@ -11,15 +12,6 @@ class Stack(Operator):
         super().__init__(model, model.graph, node, index)
         assert(self.operator_code == 'stack')
         self.setInited()
-
-
-    def compute_output_shape(self):
-        if not self.isInputShapeFullyDefined(-1):
-            self.unSupported('Illegal Input Shape.' + str(self.inputs_shape))
-            return
-
-        self.outputs_shape[0].insert(self.concat_param['axis'], len(self.reshape_param))
-        self.model.tensor_shape[self.outputs[0]] = self.outputs_shape[0]
 
 
     def parse(self):
@@ -37,25 +29,23 @@ class Stack(Operator):
         else:
             self.type = ('Reshape+' * (len(self.inputs[:-1]))) + 'Concat'
 
-            self.reshape_param = list()
-            for index, input_name in enumerate(self.inputs[:-1]):
-                inter_shape = deepcopy(self.inputs_shape[index])
-                inter_shape.insert(self.inputs_buf[-1], 1)
-                self.reshape_param.append(dict(shape=dict(dim=inter_shape)))
+            axis = self.inputs_buf[-1] if self.inputs_buf[-1] > 0 else len(self.inputs_shape[0]) + self.inputs_buf[-1] + 1
+            inter_shape = deepcopy(self.inputs_shape[0])
+            inter_shape.insert(axis, 1)
+            self.reshape_param = dict(shape=dict(dim=inter_shape))
 
             self.concat_param = dict()
             self.concat_param['axis'] = self.inputs_buf[-1]
 
             self.attrs = self.concat_param
-            self.compute_output_shape()
 
             self.setParsed()
 
 
     def convert(self):
         layers = list()
-        for i, reshap_param in enumerate(self.reshape_param):
-            layers.append(caffe_layer(self.layer_type[i], self.name[i], [self.inputs[i]], self.inputs_buf, [self.interblob[i]], reshape_param=self.reshape_param[i]))
+        for i, input_name in enumerate(self.inputs[:-1]):
+            layers.append(caffe_layer(self.layer_type[i], self.name[i], [self.inputs[i]], self.inputs_buf, [self.interblob[i]], reshape_param=self.reshape_param))
             index = i + 1
 
         layers.append(caffe_layer(self.layer_type[index], self.name[index], self.interblob, self.inputs_buf, self.outputs, concat_param=self.concat_param))
@@ -63,3 +53,11 @@ class Stack(Operator):
         self.setConverted()
 
         return layers
+
+
+    def forward(self):
+        inputs = list()
+        for input_name in self.inputs[:-1]:
+            inputs.append(self.model.variable[input_name])
+
+        return torch.stack(inputs, dim=self.inputs_buf[-1])
