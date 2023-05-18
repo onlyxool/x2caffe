3e6c1547498f797d879cd7a418b370cdf0f37625
Kyle <chenkun@vimicro.com>
2023-05-18 18:35:19 +0800

    Torch2Caffe: Update upsamplenearest forward()

--- pto2caffe/torch2caffe/op/upsamplenearest.py
+++ pto2caffe/torch2caffe/op/upsamplenearest.py
@@ -1,4 +1,5 @@
 import numpy as np
+from torch.nn.functional import interpolate
 
 from caffe_transform import caffe_layer
 from torch2caffe.op.operator import Operator
@@ -12,19 +13,8 @@ class UpsampleNearest(Operator):
         self.setInited()
 
 
-    def compute_output_shape(self):
-        if not self.isInputShapeFullyDefined(0):
-            self.unSupported('Illegal Input Shape.')
-            return
-
-        outputs_shape = self.inputs_shape[0][:2]+self.inputs_buf[1] if self.inputs_buf[1] is not None else list((np.array(self.inputs_shape[0]) * np.array([1,1]+self.inputs_buf[2])).astype(int))
-        self.model.tensor_shape[self.outputs[0]] = self.outputs_shape[0] = outputs_shape
-
-
     def parse(self):
         super().__parse__()
-
-        self.compute_output_shape()
 
         output_size = self.inputs_buf[1]
         scale_factors = self.inputs_buf[2]
@@ -40,17 +30,15 @@ class UpsampleNearest(Operator):
 
             self.convolution_param = dict()
             self.convolution_param['bias_term'] = False
-            self.convolution_param['num_output'] = self.outputs_shape[0][1]
+            self.convolution_param['num_output'] = self.inputs_shape[0][1]#self.outputs_shape[0][1]
             self.convolution_param['kernel_h'] = scale_factor_h
             self.convolution_param['kernel_w'] = scale_factor_w
             self.convolution_param['stride_h'] = int(scale_factor_h)
             self.convolution_param['stride_w'] = int(scale_factor_w)
             self.convolution_param['group'] = self.inputs_shape[0][1]
 
-            self.weight = np.ones((self.outputs_shape[0][1], 1, int(scale_factor), int(scale_factor)), dtype=int)
-            self.inputs_buf[1] = self.weight
+            self.weight = np.ones((self.inputs_shape[0][1], 1, int(scale_factor), int(scale_factor)), dtype=int)
             self.inputs_shape[1] = list(self.weight.shape)
-
 
             self.attrs = self.convolution_param
         else:
@@ -71,3 +59,8 @@ class UpsampleNearest(Operator):
         self.setConverted()
 
         return [layer]
+
+
+    def forward(self):
+        return interpolate(self.model.variable[self.inputs[0]], size=self.inputs_buf[1],
+                scale_factor=self.inputs_buf[2], mode='nearest', align_corners=None, recompute_scale_factor=None)
