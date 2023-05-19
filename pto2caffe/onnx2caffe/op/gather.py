c6e1daae81fb69dff9c280627a35d2acd4f46785
Kyle <chenkun@vimicro.com>
2023-05-19 15:46:26 +0800

    ONNX2Caffe: Fix Bug in OP Gather

--- pto2caffe/onnx2caffe/op/gather.py
+++ pto2caffe/onnx2caffe/op/gather.py
@@ -16,20 +16,18 @@ class Gather(Operator):
         if self.inputs_buf[0] is not None and self.inputs_buf[1] is not None and self.inputs_buf[1].size > 0:
             import numpy as np
             self.saveConstant(self.node.output[0], np.take(self.inputs_buf[0], indices=self.inputs_buf[1], axis=self.attrs.get('axis', 0)))
-        elif self.inputs_buf[1] is not None and self.inputs_buf[1].size == 1: #TODO
-            self.type = 'Slice'
+        elif self.inputs_buf[1] is not None and self.inputs_buf[1] == 0:
+            self.type = 'Reshape'
 
-            self.slice_param = dict()
-            self.slice_param['axis'] = self.attrs['axis']
-            self.slice_param['slice_point'] = 1
-            self.attrs = self.slice_param
+            self.reshape_param = dict(shape=dict(dim=self.outputs_shape[0]))
+            self.attrs = self.reshape_param
             self.setParsed()
         else:
             self.unSupported()
 
 
     def convert(self):
-        layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, slice_param=self.slice_param)
+        layer = caffe_layer(self.layer_type, self.name, self.inputs, self.inputs_buf, self.outputs, reshape_param=self.reshape_param)
 
         self.setConverted()
 
